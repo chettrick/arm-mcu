70aa943f0c98ff317a368027855a06bec8625839
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2007-04-20 19:04:54 +0000

    Made console serial port selectable among UART0/1/2/3, and made the default UART1
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@793 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc2366/conio.c
+++ lpc2366/conio.c
@@ -4,7 +4,7 @@
 /*                                                                            */
 /******************************************************************************/
 
-// $Id: conio.c,v 1.1 2007-03-26 18:33:25 cvs Exp $
+// $Id: conio.c,v 1.2 2007-04-20 19:04:54 cvs Exp $
 
 #include <stdio.h>
 #include <string.h>
@@ -14,25 +14,71 @@
 
 unsigned int CPUFREQ = 72000000;
 
+/* Define relocatable UART register definitions */
+
+unsigned int UARTBASE = UART1_BASE_ADDR;
+
+#define UxRBR          (*(volatile unsigned long *)(UARTBASE + 0x00))
+#define UxTHR          (*(volatile unsigned long *)(UARTBASE + 0x00))
+#define UxDLL          (*(volatile unsigned long *)(UARTBASE + 0x00))
+#define UxDLM          (*(volatile unsigned long *)(UARTBASE + 0x04))
+#define UxIER          (*(volatile unsigned long *)(UARTBASE + 0x04))
+#define UxIIR          (*(volatile unsigned long *)(UARTBASE + 0x08))
+#define UxFCR          (*(volatile unsigned long *)(UARTBASE + 0x08))
+#define UxLCR          (*(volatile unsigned long *)(UARTBASE + 0x0C))
+#define UxMCR          (*(volatile unsigned long *)(UARTBASE + 0x10))
+#define UxLSR          (*(volatile unsigned long *)(UARTBASE + 0x14))
+#define UxMSR          (*(volatile unsigned long *)(UARTBASE + 0x18))
+#define UxSCR          (*(volatile unsigned long *)(UARTBASE + 0x1C))
+#define UxACR          (*(volatile unsigned long *)(UARTBASE + 0x20))
+#define UxICR          (*(volatile unsigned long *)(UARTBASE + 0x24))
+#define UxFDR          (*(volatile unsigned long *)(UARTBASE + 0x28))
+#define UxTER          (*(volatile unsigned long *)(UARTBASE + 0x30))
+
 /* Initialize serial console */
 
 void conio_init(unsigned long int baudrate)
 {
   unsigned short int b;
 
-  PINSEL0 &= 0xFFFFFF0F;		// Enable UART 0 I/O pins
-  PINSEL0 |= 0x00000050;
+  switch (UARTBASE)
+  {
+    case UART0_BASE_ADDR :
+      PINSEL0 &= 0xFFFFFF0F;		// Enable UART 0 I/O pins
+      PINSEL0 |= 0x00000050;
+      break;
 
-  U0FDR = 0x49;				// Scale peripheral clock by 4/13
+    case UART1_BASE_ADDR :
+      PINSEL0 &= 0x3FFFFFFF;		// Enable UART 1 I/O pins
+      PINSEL0 |= 0x40000000;
+      PINSEL1 &= 0xFFFFFFFC;
+      PINSEL1 |= 0x00000001;
+      break;
+
+    case UART2_BASE_ADDR :
+      PINSEL0 &= 0xFF0FFFFF;		// Enable UART 2 I/O pins
+      PINSEL0 |= 0x00500000;
+      break;
+
+    case UART3_BASE_ADDR :
+      PINSEL0 &= 0xFFFFFFF0;		// Enable UART 3 I/O pins
+      PINSEL0 |= 0x0000000A;
+      break;
+
+    default :				// Unrecognized UART base address
+      return;				// Do nothing
+  }
+
+  UxFDR = 0x49;				// Scale peripheral clock by 4/13
 
   b = CPUFREQ/208/baudrate;
 
-  U0IER = 0x00;				// Disable UART interrupts
-  U0LCR = 0x80;
-  U0DLM = b / 256;
-  U0DLL = b % 256;
-  U0LCR = 0x03;				// Always 8 bits no parity 1 stop
-  U0FCR = 0x01;				// Enable FIFO's
+  UxIER = 0x00;				// Disable UART interrupts
+  UxLCR = 0x80;
+  UxDLM = b / 256;
+  UxDLL = b % 256;
+  UxLCR = 0x03;				// Always 8 bits no parity 1 stop
+  UxFCR = 0x01;				// Enable FIFO's
 }
 
 /* Send 1 character */
@@ -41,16 +87,16 @@ void putch(unsigned char c)
 {
   if (c == '\n') putch('\r');
 
-  while ((U0LSR & 0x20) == 0);
-  U0THR = c;
+  while ((UxLSR & 0x20) == 0);
+  UxTHR = c;
 }
 
 /* Receive 1 character */
 
 unsigned char getch(void)
 {
-  while ((U0LSR & 0x01) == 0);
-  return U0RBR;
+  while ((UxLSR & 0x01) == 0);
+  return UxRBR;
 }
 
 /* Send a string */
