3d9fe9c163ff888fe631ff3ed794748c9e68faa0
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2007-11-21 08:08:58 +0000

    Added support for STR912FA
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@877 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/cpu.h
+++ include/cpu.h
@@ -1,6 +1,6 @@
 /* Initialize CPU core */
 
-// $Id: cpu.h,v 1.2 2007-10-10 03:47:49 cvs Exp $
+// $Id: cpu.h,v 1.3 2007-11-21 08:08:58 cvs Exp $
 
 extern unsigned long int CPUFREQ;
 
@@ -20,5 +20,11 @@ extern void cpu_init(unsigned long int frequency);
 #ifdef MCU_lpc2366
 #include <lpc2366/io.h>
 #include <lpc2366/interrupt.h>
+#define DEFAULT_CPU_FREQ 48000000
+#endif
+
+#ifdef MCU_str912fa
+#include <str912fa/io.h>
+#include <str912fa/interrupt.h>
 #define DEFAULT_CPU_FREQ 48000000
 #endif
