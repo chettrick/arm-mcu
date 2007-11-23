2714d9fdc572bb22bea5b376fb4062295553a8b2
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2007-11-23 12:31:34 +0000

    Now working on STR910-EVAL, which only has P6 conveniently available
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@887 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/test_gpio.c
+++ str912faw44/test_gpio.c
@@ -1,6 +1,6 @@
 /* Simple bit twiddler test program */
 
-// $Id: test_gpio.c,v 1.1 2007-11-21 08:21:56 cvs Exp $
+// $Id: test_gpio.c,v 1.2 2007-11-23 12:31:34 cvs Exp $
 
 #include <cpu.h>
 
@@ -10,12 +10,18 @@ int main(void)
 
   cpu_init(DEFAULT_CPU_FREQ);
 
-  GPIO8_DIR = 0xFF;
-  GPIO9_DIR = 0xFF;
+  SCU_PCGR1 |= 0x00100000;	// Turn on GPIO6 clock
+  SCU_PRR1  |= 0x00100000;	// Let GPIO6 out of reset
+
+  SCU_GPIOOUT6  = 0x1555;	// P60 to P66 are GPIO outputs
+  SCU_GPIOIN6   = 0x00;		// P60 to P66 are GPIO
+  SCU_GPIOTYPE6 = 0x00;		// P60 to P66 are push pull
+
+  GPIO6_SEL = 0x00;		// P60 to P66 are GPIO
+  GPIO6_DIR = 0x7F;		// P60 to P66 are outputs
 
   for (i = 0;; i++)
   {
-    GPIO8_DATA = i;
-    GPIO9_DATA = i;
+    GPIO6_DATA = i;
   }
 }
