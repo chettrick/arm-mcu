6c72cdc21255063cb2ed5c46ace1a5c789d59c90
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2007-12-10 12:06:55 +0000

    Change CPU frequency to 48 MHz
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@894 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/cpu.c
+++ str912faw44/cpu.c
@@ -1,14 +1,21 @@
 /* Initialize CPU core */
 
-// $Id: cpu.c,v 1.2 2007-12-10 10:20:15 cvs Exp $
+// $Id: cpu.c,v 1.3 2007-12-10 12:06:55 cvs Exp $
 
 #include <cpu.h>
-#include <stdio.h>
-#include <string.h>
 
 unsigned long int CPUFREQ;
 
 void cpu_init(unsigned long int frequency)
 {
   CPUFREQ = DEFAULT_CPU_FREQ;		// Not currently changeable
+
+  SCU_CLKCNTR = 0x00020002;		// Reset SCU_CLKCNTR register
+
+  SCU_PLLCONF = 0x0003C019;		// P=3, N=192, M=25
+  SCU_PLLCONF = 0x000BC019;		// Enable PLL
+
+  while ((SCU_SYSSTATUS & 0x01) == 00);	// Wait for PLL lock
+
+  SCU_CLKCNTR &= 0xFFFFFFFC;		// Switch master clock source to PLL
 }
