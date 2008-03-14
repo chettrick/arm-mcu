ce542aedad6d00b3996c6cf25f99bdacd2cb13dc
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-03-14 21:42:34 +0000

    GCC 4.3.0 wants %d for size_t instead of %ld
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1010 06ee2f96-5407-11de-8bab-f3d7589a4122

--- at91sam7s256/test_conio.c
+++ at91sam7s256/test_conio.c
@@ -1,6 +1,6 @@
 /* Simple serial console I/O test program */
 
-// $Id: test_conio.c,v 1.3 2008-01-14 11:13:38 cvs Exp $
+// $Id: test_conio.c,v 1.4 2008-03-14 21:42:34 cvs Exp $
 
 #include <conio.h>
 #include <cpu.h>
@@ -21,6 +21,6 @@ int main(void)
     printf("Enter a string: ");
     fgets(buf, sizeof(buf), stdin);
     if (strlen(buf)) if (buf[strlen(buf)-1] == '\n') buf[strlen(buf)-1] = 0;
-    printf("You entered %ld bytes, '%s'\n", strlen(buf), buf);
+    printf("You entered %d bytes, '%s'\n", strlen(buf), buf);
   }
 }
