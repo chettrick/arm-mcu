1f6de705f3b793de4659455e6df39c1ff2d9e254
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-03-15 07:05:48 +0000

    STM32-P103 uses USART2 for console
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1014 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f103/test_conio.c
+++ stm32f103/test_conio.c
@@ -1,6 +1,6 @@
 /* Simple serial console I/O test program */
 
-// $Id: test_conio.c,v 1.1 2008-03-07 13:51:19 cvs Exp $
+// $Id: test_conio.c,v 1.2 2008-03-15 07:05:48 cvs Exp $
 
 #include <conio.h>
 #include <cpu.h>
@@ -12,7 +12,7 @@ int main(void)
   char buf[32];
 
   cpu_init(DEFAULT_CPU_FREQ);
-  conio_init(0, 19200);
+  conio_init(USART2_BASE, 19200);
 
   puts("\033[H\033[2JSTM32F103 Console I/O Test\n");
 
