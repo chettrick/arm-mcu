d2e2003e97ae3fe6d6632829376578e7cfae997b
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-03-24 08:11:18 +0000

    Cleaned up and redone based on a different sample program fro ST.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1022 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f103/cpu.c
+++ stm32f103/cpu.c
@@ -1,6 +1,6 @@
 /* Initialize CPU core */
 
-// $Id: cpu.c,v 1.1 2008-03-07 13:51:19 cvs Exp $
+// $Id: cpu.c,v 1.2 2008-03-24 08:11:18 cvs Exp $
 
 #include <cpu.h>
 
@@ -13,28 +13,31 @@ void cpu_init(unsigned long int frequency)
   CPUFREQ = DEFAULT_CPU_FREQ;		// Not currently changeable
 	
   *NVIC_CCR = *NVIC_CCR | 0x200;	/* Set STKALIGN in NVIC */
-  // 1. Cloking the controller from internal HSI RC (8 MHz)
-  RCC_HSICmd(ENABLE);
-  // wait until the HSI is ready
-  while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
-  RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
-  // 2. Enable ext. high frequency OSC
+
+// Turn on crystal oscillator
+
   RCC_HSEConfig(RCC_HSE_ON);
-  // wait until the HSE is ready
-  while(RCC_GetFlagStatus(RCC_FLAG_HSERDY) == RESET);
-  // 3. Init PLL
-  RCC_PLLConfig(RCC_PLLSource_HSE_Div1,RCC_PLLMul_9); // 72MHz
+  while (RCC_GetFlagStatus(RCC_FLAG_HSERDY) == RESET);
+
+// Configure flash timing
+
+  FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+  FLASH_SetLatency(FLASH_Latency_2);	// 2 wait states
+
+// Configure clocks
+
+  RCC_HCLKConfig(RCC_SYSCLK_Div1);	// HCLK = SYSCLK
+  RCC_PCLK1Config(RCC_HCLK_Div2);	// PCLK1 = HCLK/2
+  RCC_PCLK2Config(RCC_HCLK_Div1);	// PCLK2 = HCLK
+
+// Configure PLL for 72 MHz
+
+  RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9); // 8 MHz * 9 = 72 MHz
   RCC_PLLCmd(ENABLE);
-  // wait until the PLL is ready
   while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-  // 4. Set system clock divders
-  RCC_USBCLKConfig(RCC_USBCLKSource_PLLCLK_1Div5);
-  RCC_ADCCLKConfig(RCC_PCLK2_Div8);
-  RCC_PCLK2Config(RCC_HCLK_Div1);
-  RCC_PCLK1Config(RCC_HCLK_Div2);
-  RCC_HCLKConfig(RCC_SYSCLK_Div1);
-  // Flash 1 wait state 
-  *(vu32 *)0x40022000 = 0x12;
-  // 5. Clock system from PLL
+
+// Select PLL for SYSCLK source
+
   RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+  while(RCC_GetSYSCLKSource() != 0x08);
 }
