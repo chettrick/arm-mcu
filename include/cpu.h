523264b670d4ea9c39efb6264eeda393f0d21ed2
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-06-26 21:32:05 +0000

    Don't use io.h and interrupt.h for STR912FAW44 and STM32M103 anymore.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1091 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/cpu.h
+++ include/cpu.h
@@ -1,6 +1,6 @@
 /* Initialize CPU core */
 
-// $Id: cpu.h,v 1.7 2008-02-26 12:31:32 cvs Exp $
+// $Id: cpu.h,v 1.8 2008-06-26 21:32:05 cvs Exp $
 
 extern unsigned long int CPUFREQ;
 
@@ -24,13 +24,13 @@ extern void cpu_init(unsigned long int frequency);
 #endif
 
 #ifdef MCU_str912faw44
-#include <str912faw44/io.h>
-#include <str912faw44/interrupt.h>
+#include <str912faw44/FWLib/91x_lib.h>
+#include <str912faw44/FWLib/91x_it.h>
 #define DEFAULT_CPU_FREQ 48000000
 #endif
 
 #ifdef MCU_stm32f103
-#include <stm32f103/io.h>
-#include <stm32f103/interrupt.h>
+#include <stm32f103/FWLib/stm32f10x_lib.h>
+#include <stm32f103/FWLib/stm32f10x_it.h>
 #define DEFAULT_CPU_FREQ 72000000
 #endif
