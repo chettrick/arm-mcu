6198c73a1a33bcb273cc1997ae1c87193d77cef1
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-07-02 17:42:17 +0000

    Build libusb_serial.a
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1129 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/mcu.mk
+++ str912faw44/mcu.mk
@@ -1,11 +1,13 @@
 # Processor dependent make definitions
 
-# $Id: mcu.mk,v 1.3 2008-07-01 19:21:11 cvs Exp $
+# $Id: mcu.mk,v 1.4 2008-07-02 17:42:17 cvs Exp $
 
 ARCH		= arm9
 
 FWLIB		= $(MCUDEPENDENT)/FWLib
 CFLAGS		+= -I$(FWLIB)
+
+USBSERIAL	= $(MCUDEPENDENT)/usb_serial
 
 LIBOBJS		= conio.o cpu.o syscalls.o
 
@@ -17,11 +19,13 @@ lib$(MCU).a: $(LIBOBJS)
 	$(AR) crs lib$(MCU).a $(LIBOBJS)
 	for F in $(FWLIB)/*.c ; do $(MAKE) $${F%.c}.o ; done
 	$(AR) crs lib$(MCU).a $(FWLIB)/*.o
+	for F in $(USBSERIAL)/*.c ; do $(MAKE) $${F%.c}.o ; done
+	$(AR) crs libusb_serial.a $(USBSERIAL)/*.o
 
 # Clean out working files
 
 clean_$(MCU):
-	rm -f *.a *.o $(FWLIB)/*.o
+	rm -f *.a *.o $(FWLIB)/*.o $(USBSERIAL)/*.o
 
 # Define a suffix rule for programming the flash with OpenOCD
 
