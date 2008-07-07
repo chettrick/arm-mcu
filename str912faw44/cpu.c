02082ca7123e4e63cef6368670645a6d92f05629
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-07-07 18:15:31 +0000

    Reordered clock initialization slightly
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1134 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/cpu.c
+++ str912faw44/cpu.c
@@ -1,6 +1,6 @@
 /* Initialize CPU core */
 
-// $Id: cpu.c,v 1.6 2008-06-27 20:49:27 cvs Exp $
+// $Id: cpu.c,v 1.7 2008-07-07 18:15:31 cvs Exp $
 
 #include <cpu.h>
 
@@ -20,17 +20,17 @@ void cpu_init(unsigned long int frequency)
   SCU_PLLFactorsConfig(192, 25, 3);		// PLL = ((25 MHz/25)*2*192)/8) = 48 MHz
   SCU_PLLCmd(ENABLE);
 
+// Switch MCLK to PLL
+
+  SCU_MCLKSourceConfig(SCU_MCLK_PLL);		// MCLK = PLL = 48 MHz
+
 // Configure various subordinate clock dividers
 
   SCU_BRCLKDivisorConfig(SCU_BRCLK_Div1);	// BRCLK = MCLK = 48 MHz
   SCU_RCLKDivisorConfig(SCU_RCLK_Div1);		// RCLK = MCLK = 48 MHz
-  SCU_USBCLKConfig(SCU_USBCLK_MCLK);		// USBCLK = MCLK/2 = 48 MHz
+  SCU_USBCLKConfig(SCU_USBCLK_MCLK);		// USBCLK = MCLK = 48 MHz
 
   SCU_FMICLKDivisorConfig(SCU_FMICLK_Div1);	// FMICLK = RCLK = 48 MHz
   SCU_HCLKDivisorConfig(SCU_HCLK_Div1);		// HCLK = RCLK = 48 MHz
   SCU_PCLKDivisorConfig(SCU_PCLK_Div1);		// PCLK = RCLK = 48 MHz
-
-// Switch MCLK to PLL
-
-  SCU_MCLKSourceConfig(SCU_MCLK_PLL);		// MCLK = PLL
 }
