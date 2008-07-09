da059a390020c778d68978abe9ff903f9c89e453
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-07-09 18:57:13 +0000

    usb_serial_send() now returns the number of bytes sent, or zero if the USB transmitter is busy.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1151 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/usb_serial/usb_serial.h
+++ str912faw44/usb_serial/usb_serial.h
@@ -1,6 +1,6 @@
 /* USB serial port encapsulation routines */
 
-// $Id: usb_serial.h,v 1.2 2008-07-09 15:13:33 cvs Exp $
+// $Id: usb_serial.h,v 1.3 2008-07-09 18:57:13 cvs Exp $
 
 // Connect or disconnect D+ pullup resistor
 
@@ -12,7 +12,7 @@ void usb_serial_init(void);
 
 // Send data to USB host
 
-void usb_serial_send(void *buf, int len);
+int usb_serial_send(void *buf, int len);
 
 // Receive data from USB host
 
