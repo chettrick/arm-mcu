12a755477e751610c44ddf585dde4c9681fdfd4a
cvs <cvs@06ee2f96-5407-11de-8bab-f3d7589a4122>
2008-07-10 20:22:21 +0000

    Include usb_type.h. Define __USB_SERIAL_H.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1153 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/usb_serial/usb_serial.h
+++ str912faw44/usb_serial/usb_serial.h
@@ -1,6 +1,11 @@
 /* USB serial port encapsulation routines */
 
-// $Id: usb_serial.h,v 1.3 2008-07-09 18:57:13 cvs Exp $
+// $Id: usb_serial.h,v 1.4 2008-07-10 20:22:21 cvs Exp $
+
+#ifndef __USB_SERIAL_H
+#define __USB_SERIAL_H
+
+#include <usb_type.h>
 
 // Connect or disconnect D+ pullup resistor
 
@@ -17,3 +22,4 @@ int usb_serial_send(void *buf, int len);
 // Receive data from USB host
 
 int usb_serial_receive(void *buf, int bufsize);
+#endif
