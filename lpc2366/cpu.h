cb8d4331b00b8692c2bbeec84f7b1ad72dd30f10
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2009-09-25 10:37:33 +0000

    Renamed io.h back to LPC23XX.h
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1645 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc2366/cpu.h
+++ lpc2366/cpu.h
@@ -7,9 +7,10 @@ extern unsigned long int CPUFREQ;
 extern void cpu_init(unsigned long int frequency);
 
 #include <device.h>
-#include <io.h>
 #include <interrupt.h>
 #include <serial.h>
+
+#include <LPC23XX.h>
 
 #ifndef DEFAULT_CPU_FREQ
 #define DEFAULT_CPU_FREQ	48000000
