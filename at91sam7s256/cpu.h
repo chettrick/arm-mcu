cb2dbf44970e36451c504a36eb9a86b02e250ed7
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2009-09-25 10:40:01 +0000

    Cosmetic reordering of header includes This line, and those below, will be ignored--
    M    at91sam7s256/cpu.h
    
    
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@1646 06ee2f96-5407-11de-8bab-f3d7589a4122

--- at91sam7s256/cpu.h
+++ at91sam7s256/cpu.h
@@ -6,9 +6,10 @@ extern unsigned long int CPUFREQ;
 
 extern void cpu_init(unsigned long int frequency);
 
-#include <AT91SAM7S256.h>
 #include <device.h>
 #include <serial.h>
+
+#include <at91lib/AT91SAM7S256.h>
 
 #ifndef DEFAULT_CPU_FREQ
 #define DEFAULT_CPU_FREQ	48000000
