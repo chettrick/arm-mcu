24eb221a3edf9cddcc3c60e65052ecf2f077e2e8
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-02-04 21:53:48 +0000

    Added support for MBED_LPC2368
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2323 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc2366/test_gpio.c
+++ lpc2366/test_gpio.c
@@ -14,6 +14,7 @@ int main(void)
 
   PCLKSEL1 = 0x00000004;	// GPIO peripheral clock is CCLK/1
 
+#ifdef BOARD_OLIMEX_LPC_P2378
   PINSEL0 = 0x00000000;		// Every P0 pin is a GPIO
   PINSEL1 = 0x00000000;
 
@@ -22,4 +23,16 @@ int main(void)
 
   for (i = 0;; i++)
     FIO0PIN = i;
+#endif
+
+#ifdef BOARD_MBED_LPC2368
+  PINSEL2 = 0x00000000;         // Every P1 pin is a GPIO
+  PINSEL3 = 0x00000000;
+
+  FIO1MASK = 0x00000000;        // Every P1 pin is unmasked
+  FIO1DIR = 0xFFFFFFFF;         // Every P1 pin is an output
+
+  for (i = 0;; i++)
+    FIO1PIN = i;
+#endif
 }
