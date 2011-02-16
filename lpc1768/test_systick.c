ea9dff6af3988fe95db69e13c4ddf33b83e41962
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-02-16 12:26:02 +0000

    Checked into repository
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2383 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ lpc1768/test_systick.c
@@ -0,0 +1,54 @@
+/* Simple timer interrupt test program */
+
+// $Id$
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <stdio.h>
+#include "lpc17xx_systick.h"
+#include "lpc17xx_uart.h"
+
+#define TRUE	1
+#define	FALSE	0
+
+volatile int TimerCounter = 0;
+volatile int TimerFlag = FALSE;
+
+void SysTick_Handler(void)
+{
+  SYSTICK_ClearCounterFlag();
+
+  if (++TimerCounter == 10)
+  {
+    TimerCounter = 0;
+    TimerFlag = TRUE;
+  }
+}
+
+int main(void)
+{
+  cpu_init(DEFAULT_CPU_FREQ);
+  serial_stdio(CONSOLE_PORT, 115200);
+
+  puts("\033[H\033[2JLPC1768 System Tick Interrupt Test ("
+       __DATE__ " " __TIME__ ")\n");
+
+// Initialize System Tick with 100ms time interval
+
+  SYSTICK_InternalInit(100);
+  SYSTICK_IntCmd(ENABLE);
+  SYSTICK_Cmd(ENABLE);
+
+// Display "Tick..." every second
+
+  for (;;)
+  {
+    if (TimerFlag)
+    {
+      TimerFlag = FALSE;
+      puts("Tick...");
+      fflush(stdout);
+    }
+  }
+}
