279a3f789beaca4fd3f8a87c31846a38845a5346
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-03-05 16:42:34 +0000

    Checked into repository
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2433 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ stm32f103/test_float.c
@@ -0,0 +1,33 @@
+/* Simple floating point arithmetic test program for the STM32F103 ARM MCU */
+
+// $Id$
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef __ARM_EABI__
+#error arm-elf-gcc does not support Cortex-M3 floating point correctly! Use an EABI toolchain.
+#endif
+
+int main(void)
+{
+  float x, y;
+  int z;
+
+  cpu_init(DEFAULT_CPU_FREQ);
+
+  serial_stdio(CONSOLE_PORT, 115200);
+
+  puts("\033[H\033[2JSTM32F103 Floating Point Arithmetic Test (" __DATE__ " " __TIME__ ")\n");
+  puts(revision);
+  printf("\nCPU Freq:%ld Hz  Compiler:%s\n\n", CPUFREQ, __VERSION__);
+
+  x = 1.0;
+  y = 2.0;
+  z = x + y;
+
+  printf("x is %f, y is %f, x+y is %f, z is %d\n", x, y, x+y, z);
+}
