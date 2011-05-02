e6d3e979ef89a95d2cd1344d70de8e0429e08c81
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-05-02 12:35:14 +0000

    Added support for BlueBoard LPC1768-H
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2565 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc1768/test_gpio.c
+++ lpc1768/test_gpio.c
@@ -12,6 +12,8 @@ int main(void)
 
   cpu_init(DEFAULT_CPU_FREQ);
 
+// Flash MBED-LPC1768 LED's
+
 #ifdef BOARD_MBED_LPC1768
 #define LED1	18
 #define LED2	20
@@ -23,7 +25,21 @@ int main(void)
   LPC_GPIO1->FIOMASK = !LEDMASK;
   LPC_GPIO1->FIODIR = LEDMASK;
 
-  for (i = 0;; i ++)
+  for (i = 0;; i++)
     LPC_GPIO1->FIOPIN = i;
+#endif
+
+// Flash BlueBoard LPC1768-H test LED
+
+#ifdef BOARD_BLUEBOARD_LPC1768_H
+#define LED1	29
+
+#define LEDMASK	((1 << LED1))
+
+  LPC_GPIO1->FIOMASK = !LEDMASK;
+  LPC_GPIO1->FIODIR = LEDMASK;
+
+  for (i = 0;; i++)
+    LPC_GPIO1->FIOPIN = i << 7;
 #endif
 }
