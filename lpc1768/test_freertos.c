1a06efc5c97580cfb9805ca789eee71bc9b62d41
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-05-25 10:41:15 +0000

    Added code to create 2 tasks
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2695 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc1768/test_freertos.c
+++ lpc1768/test_freertos.c
@@ -7,11 +7,30 @@ static const char revision[] = "$Id$";
 #include <assert.h>
 #include <cpu.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <FreeRTOS.h>
 #include <task.h>
 
+void TaskFunction(void *parameters)
+{
+  char *message = parameters;
+  unsigned int seed = (unsigned int) &seed;
+
+  portTickType waketime = xTaskGetTickCount();
+
+  for (;;)
+  {
+    serial_write(CONSOLE_PORT, message, strlen(message));
+    vTaskDelayUntil(&waketime, (1000 + (rand_r(&seed) % 500))/portTICK_RATE_MS);    
+  }
+}
+
 int main(void)
 {
+  xTaskHandle task1;
+
   cpu_init(DEFAULT_CPU_FREQ);
 
   serial_stdio(CONSOLE_PORT, 115200);
@@ -21,11 +40,27 @@ int main(void)
   printf("\nCPU Freq:%ld Hz  Compiler:%s  FreeRTOS:%s\n\n", CPUFREQ, __VERSION__,
     tskKERNEL_VERSION_NUMBER);
 
+  if (xTaskCreate(TaskFunction, (signed char *) "Task 1", 240, "This is Task 1\r\n", 1, &task1) != pdPASS)
+  {
+    puts("ERROR: xTaskCreate() for task 1 failed!");
+    fflush(stdout);
+    assert(0);
+  }
+
+  if (xTaskCreate(TaskFunction, (signed char *) "Task 2", 240, "This is Task 2\r\n", 1, &task1) != pdPASS)
+  {
+    puts("ERROR: xTaskCreate() for task 2 failed!");
+    fflush(stdout);
+    assert(0);
+  }
+
   vTaskStartScheduler();
   assert(0);
 }
 
 void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed char *pcTaskName)
 {
+  printf("ERROR: vApplicationStackOverflowHook(): Task \"%s\" overflowed its stack\n", pcTaskName);
+  fflush(stdout);
   assert(0);
 }
