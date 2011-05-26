9b39799e8537bf4d3ac47be1f43f1bb447acbf69
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-05-26 09:13:42 +0000

    Added support for FreeRTOS
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2722 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str912faw44/mcu.mk
+++ str912faw44/mcu.mk
@@ -8,7 +8,9 @@ BOARDNAME	?= STMICRO_STR910_EVAL
 
 FWLIB		= $(MCUDEPENDENT)/FWLib
 USBSERIAL	= $(MCUDEPENDENT)/usb_serial
-CFLAGS		+= -I$(FWLIB) -I$(USBSERIAL)
+FREERTOS_DIR	= $(MCUDEPENDENT)/FreeRTOS
+
+CFLAGS		+= -I$(FWLIB) -I$(USBSERIAL) -I$(FREERTOS_DIR)
 
 LIBOBJS		= cpu.o device.o serial.o syscalls.o time.o
 
@@ -27,6 +29,8 @@ lib$(MCU).a: $(LIBOBJS)
 	$(AR) crs lib$(MCU).a $(FWLIB)/*.o
 	for F in $(USBSERIAL)/*.c ; do $(MAKE) $${F%.c}.o ; done
 	$(AR) crs lib$(MCU).a $(USBSERIAL)/*.o
+	for F in $(FREERTOS_DIR)/*.c ; do $(MAKE) $${F%.c}.o ; done
+	$(AR) crs lib$(MCU).a $(FREERTOS_DIR)/*.o
 
 lib: lib$(MCU).a
 
