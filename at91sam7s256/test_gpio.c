63d94d7a2d0ad1df0affe2ac0878ffbb49a8128b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-03 07:11:50 +0000

    Configure LED pins specifically for Olimex SAM7-P256 board
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2752 06ee2f96-5407-11de-8bab-f3d7589a4122

--- at91sam7s256/test_gpio.c
+++ at91sam7s256/test_gpio.c
@@ -15,14 +15,33 @@ int main(void)
 /* Configure GPIO's */
 
   *AT91C_PMC_PCER	= 0x00000004;	// Enable Port A peripheral clock
+  *AT91C_PIOA_PER	= 0xFFFFFFFF;	// All GPIO pins are GPIO's
+  *AT91C_PIOA_PPUER	= 0xFFFFFFFF;	// All GPIO pins have internal pullup
+  *AT91C_PIOA_ODR	= 0xFFFFFFFF;	// All GPIO pins are inputs
+  *AT91C_PIOA_OWDR	= 0xFFFFFFFF;	// All GPIO pins are not synchronous
+  *AT91C_PIOA_MDDR	= 0xFFFFFFFF;	// All GPIO pins are push-pull
 
-  *AT91C_PIOA_PER	= 0xFFFFFFFF;	// Port A is all GPIO
-  *AT91C_PIOA_OER	= 0xFFFFFFFF;	// Port A is all output
-  *AT91C_PIOA_OWER	= 0xFFFFFFFF;	// Port A all ODSR enabled
-  *AT91C_PIOA_MDDR	= 0xFFFFFFFF;	// Port A all push pull
+#ifdef BOARD_OLIMEX_SAM7_P256
+#define LED1		18
+#define LED2		17
+
+  *AT91C_PIOA_PER	= 1 << LED1;	// LED1 pin is GPIO
+  *AT91C_PIOA_PPUDR	= 1 << LED1;	// LED1 pin does not need internal pullup
+  *AT91C_PIOA_OER	= 1 << LED1;	// LED1 pin is output
+  *AT91C_PIOA_OWER	= 1 << LED1;	// LED1 pin is synchronous output
+  *AT91C_PIOA_MDER	= 1 << LED1;	// LED1 pin is open drain output
+
+  *AT91C_PIOA_PER	= 1 << LED2;	// LED2 pin is GPIO
+  *AT91C_PIOA_PPUDR	= 1 << LED2;	// LED2 pin does not need internal pullup
+  *AT91C_PIOA_OER	= 1 << LED2;	// LED2 pin is output
+  *AT91C_PIOA_OWER	= 1 << LED2;	// LED2 pin is synchronous output
+  *AT91C_PIOA_MDER	= 1 << LED2;	// LED2 pin is open drain output
+#endif
 
 /* Trivial main program */
 
-  for (i = 0xFFFFFFFF;; i++)
-    *AT91C_PIOA_ODSR = i;		// Update PORT B outputs
+  for (i = 0;; i++)
+  {
+    *AT91C_PIOA_ODSR = i >> 2;		// Update LED's
+  }
 }
