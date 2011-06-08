be692342530c7e5d530a603e65bbd99bb55eb39a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-08 15:29:18 +0000

    Display revisin and CPU freq at startup
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2855 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc17xx/test_systick.c
+++ lpc17xx/test_systick.c
@@ -29,8 +29,9 @@ int main(void)
   cpu_init(DEFAULT_CPU_FREQ);
   serial_stdio(CONSOLE_PORT, 115200);
 
-  puts("\033[H\033[2JLPC1768 System Tick Interrupt Test ("
-       __DATE__ " " __TIME__ ")\n");
+  puts("\033[H\033[2JLPC1768 System Tick Interrupt Test (" __DATE__ " " __TIME__ ")\n");
+  puts(revision);
+  printf("\nCPU Freq:%ld Hz  Compiler:%s\n\n", CPUFREQ, __VERSION__);
 
 // Initialize System Tick with 100ms time interval
 
