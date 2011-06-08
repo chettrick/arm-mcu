4f99473e2cacbe5eb8875a6937dc8990ec66c00f
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-08 12:28:57 +0000

    Added to repository
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2844 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ efm32/test_systick.c
@@ -0,0 +1,71 @@
+/* Simple timer interrupt test program */
+
+// $Id$
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <stdio.h>
+#include <efm32_cmu.h>
+#include <efm32_gpio.h>
+
+#define TRUE	1
+#define	FALSE	0
+
+volatile int TimerCounter = 0;
+volatile int TimerFlag = FALSE;
+
+void SysTick_Handler(void)
+{
+  if (++TimerCounter == 10)
+  {
+    TimerCounter = 0;
+    TimerFlag = TRUE;
+  }
+}
+
+int main(void)
+{
+  cpu_init(DEFAULT_CPU_FREQ);
+  serial_stdio(CONSOLE_PORT, 115200);
+
+  puts("\033[H\033[2JEFM32 System Tick Interrupt Test ("
+       __DATE__ " " __TIME__ ")\n");
+
+// Configure LED(s)
+
+#ifdef BOARD_EFM32_G8XX_STK
+  /* Enable GPIO */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  /* Configure GPIO port C 0-3 as LED control outputs */
+  GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 1);
+  GPIO_PinModeSet(gpioPortC, 1, gpioModePushPull, 1);
+  GPIO_PinModeSet(gpioPortC, 2, gpioModePushPull, 1);
+  GPIO_PinModeSet(gpioPortC, 3, gpioModePushPull, 1);
+
+  /* Set initial LED states */
+  GPIO_PortOutSetVal(gpioPortC, 0x05, 0xf);
+#endif
+
+// Initialize System Tick with 100ms time interval
+
+  SysTick_Config(SystemCoreClock / 10);
+
+// Display "Tick..." every second
+
+  for (;;)
+  {
+    if (TimerFlag)
+    {
+      TimerFlag = FALSE;
+
+#ifdef BOARD_EFM32_G8XX_STK
+      GPIO_PortOutSetVal(gpioPortC, GPIO_PortOutGet(gpioPortC), 0xf);	// Toggle LEDs
+#endif
+
+      puts("Tick...");
+      fflush(stdout);
+    }
+  }
+}
