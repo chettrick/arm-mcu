c92e07aad6e51d00b0202e8cbb262bc65b1ff595
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-08 11:08:30 +0000

    Extracted from EFM32_CMSIS_2.0.0.zip
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2836 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ efm32/CMSIS/include/efm32.h
@@ -0,0 +1,226 @@
+/**************************************************************************//**
+ * @file
+ * @brief CMSIS Cortex-M0/M3 Peripheral Access Layer for EFM32 device series
+ *
+ * This is a convenience header file for defining the EFM32 part number on the
+ * build command line, instead of specifying the part specific header file.
+ * @verbatim
+ * Example: Add "-DEFM32G890F128" to your build options, to define part
+ *          Add "#include "efm32.h" to your source files
+ * @endverbatim
+ * @author Energy Micro AS
+ * @version 2.0.0
+ ******************************************************************************
+ * @section License
+ * <b>(C) Copyright 2011 Energy Micro AS, http://www.energymicro.com</b>
+ ******************************************************************************
+ *
+ * This source code is the property of Energy Micro AS. The source and compiled
+ * code may only be used on Energy Micro "EFM32" microcontrollers.
+ *
+ * This copyright notice may not be removed from the source code nor changed.
+ *
+ * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Energy Micro AS has no
+ * obligation to support this Software. Energy Micro AS is providing the
+ * Software "AS IS", with no express or implied warranties of any kind,
+ * including, but not limited to, any implied warranties of merchantability
+ * or fitness for any particular purpose or warranties against infringement
+ * of any proprietary rights of a third party.
+ *
+ * Energy Micro AS will not be liable for any consequential, incidental, or
+ * special damages, or any other relief, or for any claim by any third party,
+ * arising from your use of this Software.
+ *
+ *****************************************************************************/
+
+#ifndef __EFM32_H
+#define __EFM32_H
+
+
+#if defined(EFM32G200F16)
+#include "efm32g200f16.h"
+
+#elif defined(EFM32G200F32)
+#include "efm32g200f32.h"
+
+#elif defined(EFM32G200F64)
+#include "efm32g200f64.h"
+
+#elif defined(EFM32G210F128)
+#include "efm32g210f128.h"
+
+#elif defined(EFM32G222F128)
+#include "efm32g222f128.h"
+
+#elif defined(EFM32G222F32)
+#include "efm32g222f32.h"
+
+#elif defined(EFM32G222F64)
+#include "efm32g222f64.h"
+
+#elif defined(EFM32G230F128)
+#include "efm32g230f128.h"
+
+#elif defined(EFM32G230F32)
+#include "efm32g230f32.h"
+
+#elif defined(EFM32G230F64)
+#include "efm32g230f64.h"
+
+#elif defined(EFM32G232F128)
+#include "efm32g232f128.h"
+
+#elif defined(EFM32G232F32)
+#include "efm32g232f32.h"
+
+#elif defined(EFM32G232F64)
+#include "efm32g232f64.h"
+
+#elif defined(EFM32G280F128)
+#include "efm32g280f128.h"
+
+#elif defined(EFM32G280F32)
+#include "efm32g280f32.h"
+
+#elif defined(EFM32G280F64)
+#include "efm32g280f64.h"
+
+#elif defined(EFM32G290F128)
+#include "efm32g290f128.h"
+
+#elif defined(EFM32G290F32)
+#include "efm32g290f32.h"
+
+#elif defined(EFM32G290F64)
+#include "efm32g290f64.h"
+
+#elif defined(EFM32G840F128)
+#include "efm32g840f128.h"
+
+#elif defined(EFM32G840F32)
+#include "efm32g840f32.h"
+
+#elif defined(EFM32G840F64)
+#include "efm32g840f64.h"
+
+#elif defined(EFM32G842F128)
+#include "efm32g842f128.h"
+
+#elif defined(EFM32G842F32)
+#include "efm32g842f32.h"
+
+#elif defined(EFM32G842F64)
+#include "efm32g842f64.h"
+
+#elif defined(EFM32G880F128)
+#include "efm32g880f128.h"
+
+#elif defined(EFM32G880F32)
+#include "efm32g880f32.h"
+
+#elif defined(EFM32G880F64)
+#include "efm32g880f64.h"
+
+#elif defined(EFM32G890F128)
+#include "efm32g890f128.h"
+
+#elif defined(EFM32G890F32)
+#include "efm32g890f32.h"
+
+#elif defined(EFM32G890F64)
+#include "efm32g890f64.h"
+
+#elif defined(EFM32TG108F16)
+#include "efm32tg108f16.h"
+
+#elif defined(EFM32TG108F32)
+#include "efm32tg108f32.h"
+
+#elif defined(EFM32TG108F4)
+#include "efm32tg108f4.h"
+
+#elif defined(EFM32TG108F8)
+#include "efm32tg108f8.h"
+
+#elif defined(EFM32TG110F16)
+#include "efm32tg110f16.h"
+
+#elif defined(EFM32TG110F32)
+#include "efm32tg110f32.h"
+
+#elif defined(EFM32TG110F4)
+#include "efm32tg110f4.h"
+
+#elif defined(EFM32TG110F8)
+#include "efm32tg110f8.h"
+
+#elif defined(EFM32TG210F16)
+#include "efm32tg210f16.h"
+
+#elif defined(EFM32TG210F32)
+#include "efm32tg210f32.h"
+
+#elif defined(EFM32TG210F8)
+#include "efm32tg210f8.h"
+
+#elif defined(EFM32TG222F16)
+#include "efm32tg222f16.h"
+
+#elif defined(EFM32TG222F32)
+#include "efm32tg222f32.h"
+
+#elif defined(EFM32TG222F8)
+#include "efm32tg222f8.h"
+
+#elif defined(EFM32TG230F16)
+#include "efm32tg230f16.h"
+
+#elif defined(EFM32TG230F32)
+#include "efm32tg230f32.h"
+
+#elif defined(EFM32TG230F8)
+#include "efm32tg230f8.h"
+
+#elif defined(EFM32TG232F16)
+#include "efm32tg232f16.h"
+
+#elif defined(EFM32TG232F32)
+#include "efm32tg232f32.h"
+
+#elif defined(EFM32TG232F8)
+#include "efm32tg232f8.h"
+
+#elif defined(EFM32TG822F16)
+#include "efm32tg822f16.h"
+
+#elif defined(EFM32TG822F32)
+#include "efm32tg822f32.h"
+
+#elif defined(EFM32TG822F8)
+#include "efm32tg822f8.h"
+
+#elif defined(EFM32TG840F16)
+#include "efm32tg840f16.h"
+
+#elif defined(EFM32TG840F32)
+#include "efm32tg840f32.h"
+
+#elif defined(EFM32TG840F8)
+#include "efm32tg840f8.h"
+
+#elif defined(EFM32TG842F16)
+#include "efm32tg842f16.h"
+
+#elif defined(EFM32TG842F32)
+#include "efm32tg842f32.h"
+
+#elif defined(EFM32TG842F8)
+#include "efm32tg842f8.h"
+
+
+#else
+#error "efm32.h: PART NUMBER undefined"
+#endif
+
+#endif
+
