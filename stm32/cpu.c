d0945091d51b0c3e97f81a9816b4a5c5fdb41a07
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-13 07:33:22 +0000

    Just call SystemInit() to initialize clocks
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2899 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32/cpu.c
+++ stm32/cpu.c
@@ -6,62 +6,8 @@ static const char revision[] = "$Id$";
 
 #include <cpu.h>
 
-#define NVIC_CCR ((volatile unsigned long *)(0xE000ED14))
-
 void cpu_init(unsigned long int frequency)
 {
-  *NVIC_CCR = *NVIC_CCR | 0x200;	/* Set STKALIGN in NVIC */
-
-// Interrupt vectors are in flash
-
-  NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);
-
-// Turn on crystal oscillator
-
-  RCC_HSEConfig(RCC_HSE_ON);
-  while (RCC_GetFlagStatus(RCC_FLAG_HSERDY) == RESET);
-
-// Configure flash timing
-
-#ifdef stm32f103rb
-  FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-  FLASH_SetLatency(FLASH_Latency_2);	// 2 wait states
-#endif
-
-// Configure subsystem clock dividers
-
-#ifdef stm32f100rb
-  RCC_HCLKConfig(RCC_SYSCLK_Div1);	// HCLK = SYSCLK
-  RCC_PCLK1Config(RCC_HCLK_Div1);	// PCLK1 = HCLK
-  RCC_PCLK2Config(RCC_HCLK_Div1);	// PCLK2 = HCLK
-#endif
-
-#ifdef stm32f103rb
-  RCC_HCLKConfig(RCC_SYSCLK_Div1);	// HCLK = SYSCLK
-  RCC_PCLK1Config(RCC_HCLK_Div2);	// PCLK1 = HCLK/2
-  RCC_PCLK2Config(RCC_HCLK_Div1);	// PCLK2 = HCLK
-#endif
-
-#ifdef stm32f100rb
-// Configure PLL for 24 MHz
-
-  RCC_PREDIV1Config(RCC_PREDIV1_Source_HSE, RCC_PREDIV1_Div2);
-  RCC_PLLConfig(RCC_PLLSource_PREDIV1, RCC_PLLMul_6);
-#endif
-
-#ifdef stm32f103rb
-// Configure PLL for 72 MHz
-
-  RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9); // 8 MHz * 9 = 72 MHz
-#endif
-
-  RCC_PLLCmd(ENABLE);
-  while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-
-// Select PLL for SYSCLK source
-
-  RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-  while(RCC_GetSYSCLKSource() != 0x08);
-
+  SystemInit();
   SystemCoreClockUpdate();
 }
