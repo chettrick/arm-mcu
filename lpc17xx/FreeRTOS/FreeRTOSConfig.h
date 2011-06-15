52c1b58cdf9da15a1a44cbededf554ededc16df5
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-06-15 08:28:46 +0000

    Enable counting semaphores
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2915 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc17xx/FreeRTOS/FreeRTOSConfig.h
+++ lpc17xx/FreeRTOS/FreeRTOSConfig.h
@@ -81,10 +81,8 @@
 #define configIDLE_SHOULD_YIELD		0
 #define configUSE_CO_ROUTINES 		0
 #define configUSE_MUTEXES		1
-
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
-
-#define configUSE_COUNTING_SEMAPHORES 	0
+#define configUSE_COUNTING_SEMAPHORES 	1
 #define configUSE_ALTERNATIVE_API 	0
 #define configCHECK_FOR_STACK_OVERFLOW	2
 #define configUSE_RECURSIVE_MUTEXES	1
