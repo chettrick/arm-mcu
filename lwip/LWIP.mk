2fcc37c992a8daee60d387525499c451480f167c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-07-15 19:31:02 +0000

    Added lwip_lib and lwip_clean targets.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@2958 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lwip/LWIP.mk
+++ lwip/LWIP.mk
@@ -8,6 +8,8 @@ LWIP_SRC	= $(LWIP_DIR)/lwip-$(LWIP_VERSION)/src
 LWIP_INC	= $(LWIP_SRC)/include
 CFLAGS		+= -I$(LWIP_DIR) -I$(LWIP_INC) -I$(LWIP_INC)/ipv4
 
+.PHONY: lwip_lib lwip_clean
+
 LWIP_OBJS	= $(LWIP_SRC)/core/mem.o		\
 		  $(LWIP_SRC)/core/memp.o		\
 		  $(LWIP_SRC)/core/netif.o		\
@@ -28,4 +30,14 @@ LWIP_OBJS	= $(LWIP_SRC)/core/mem.o		\
 		  $(LWIP_SRC)/core/ipv4/ip_addr.o	\
 		  $(LWIP_SRC)/core/ipv4/ip_frag.o	\
 		  $(LWIP_SRC)/core/ipv4/inet_chksum.o	\
-		  $(LWIP_SRC)/netif/etharp.o
+		  $(LWIP_SRC)/netif/etharp.o		\
+
+# Add LWIP object files to the MCU library
+
+lwip_lib: $(LWIP_OBJS)
+	$(AR) crs lib$(MCU).a $(LWIP_OBJS)
+
+# Remove LWIP object files
+
+lwip_clean:
+	rm -f $(LWIP_OBJS)
