27858ecd674e837a663c0932fb8b178f7a643879
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-27 07:40:24 +0000

    Added wiznet.h
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3064 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ wiznet/wiznet.h
@@ -0,0 +1,17 @@
+/* WizNet Driver Function Prototypes */
+
+// $Id$
+
+#include <W5200.h>
+
+#if defined(W5200)
+#define wiznet_initialize(x)			W5200_initialize(x)
+#define wiznet_set_hardware_address(x)		W5200_set_hardware_address(x)
+#define wiznet_get_hardware_address(x)		W5200_get_hardware_address(x)
+#define wiznet_configure_network(x,y,z)		W5200_configure_network(x,y,z)
+#define wiznet_get_ipaddress(x)			W5200_get_ipaddress(x)
+#define wiznet_get_linkstate(x)			W5200_get_linkstate(x)
+#define wiznet_tick()				W5200_tick()
+#else
+#error WizNet device must be selected by defining WIZNET_DEVICE
+#endif
