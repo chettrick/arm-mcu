74e171b5179150f767a80f2e22f8b11d1b412ff7
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-30 10:28:56 +0000

    Added socket register constants
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3082 06ee2f96-5407-11de-8bab-f3d7589a4122

--- wiznet/W5200.h
+++ wiznet/W5200.h
@@ -26,6 +26,8 @@
 
 // Socket Registers
 
+#define W5200_MAX_SOCKETS	8
+
 #define W5200_SOCKET_REG(s)	(0x4000 + (s << 8))
 
 #define W5200_Sn_MR(s)		(W5200_SOCKET_REG(s) + 0x00)
@@ -65,6 +67,43 @@
 
 #define W5200_PSTATUS_LINK	0x20
 #define W5200_PSTATUS_POWERDOWN	0x08
+
+#define W5200_Sn_MR_MULTI	0x80
+#define W5200_Sn_MR_MF		0x40
+#define W5200_Sn_MR_ND		0x20
+#define W5200_Sn_MR_MC		0x20
+#define W5200_Sn_MR_CLOSED	0x00
+#define W5200_Sn_MR_TCP		0x01
+#define W5200_Sn_MR_UDP		0x02
+#define W5200_Sn_MR_IPRAW	0x03
+#define W5200_Sn_MR_MACRAW	0x04
+
+#define W5200_Sn_CR_OPEN	0x01
+#define W5200_Sn_CR_LISTEN	0x02
+#define W5200_Sn_CR_CONNECT	0x04
+#define W5200_Sn_CR_DISCON	0x08
+#define W5200_Sn_CR_CLOSE	0x10
+#define W5200_Sn_CR_SEND	0x20
+#define W5200_Sn_CR_SEND_MAC	0x21
+#define W5200_Sn_CR_SEND_KEEP	0x22
+#define W5200_Sn_CR_RECV	0x40
+
+#define W5200_Sn_SR_SOCK_CLOSED		0x00
+#define W5200_Sn_SR_SOCK_ARP		0x01
+#define W5200_Sn_SR_SOCK_INIT		0x13
+#define W5200_Sn_SR_SOCK_LISTEN		0x14
+#define W5200_Sn_SR_SOCK_SYNSENT	0x15
+#define W5200_Sn_SR_SOCK_SYNRECV	0x16
+#define W5200_Sn_SR_SOCK_ESTABLISHED	0x17
+#define W5200_Sn_SR_SOCK_FIN_WAIT	0x18
+#define W5200_Sn_SR_SOCK_CLOSING	0x1A
+#define W5200_Sn_SR_SOCK_TIME_WAIT	0x1B
+#define W5200_Sn_SR_SOCK_CLOSE_WAIT	0x1C
+#define W5200_Sn_SR_SOCK_LAST_ACK	0x1D
+#define W5200_Sn_SR_SOCK_UDP		0x22
+#define W5200_Sn_SR_SOCK_IPRAW		0x32
+#define W5200_Sn_SR_SOCK_MACRAW		0x42
+#define W5200_Sn_SR_SOCK_PPPOE		0x5F
 
 // W5200 driver functions
 
