a8c15d9f5b3f57c4487c2da41af6e55060a08b21
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-09-30 10:31:41 +0000

    Added W5200_udp_open()
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3084 06ee2f96-5407-11de-8bab-f3d7589a4122

--- wiznet/W5200.c
+++ wiznet/W5200.c
@@ -12,6 +12,12 @@ static const char revision[] = "$Id$";
 static uint32_t spiport;
 static volatile uint32_t delaycounter = 0;
 
+void W5200_tick(void)
+{
+  if (delaycounter)
+    delaycounter--;
+}
+
 int W5200_write_register(const uint16_t address, const uint8_t data)
 {
   uint8_t txbuf[5];
@@ -40,6 +46,7 @@ int W5200_read_register(const uint16_t address, uint8_t *data)
 int W5200_initialize(const uint32_t spiportnum)
 {
   int status = 0;
+  int s;
 
 // Save SPI port number
 
@@ -47,8 +54,47 @@ int W5200_initialize(const uint32_t spiportnum)
 
 // Initialize mode register
 
-  if ((status = W5200_write_register(W5200_MR, 0)))
+  if ((status = W5200_write_register(W5200_MR, 0)))		// Disable ping block
     return status;
+
+// Initialize interrupt mask register
+
+  if ((status = W5200_write_register(W5200_IMR, 0)))		// Disable common interrupts
+    return status;
+
+// Initialize retry time
+
+  if ((status = W5200_write_register(W5200_RTR+0, 2000 / 256)))	// 200 ms
+    return status;
+
+  if ((status = W5200_write_register(W5200_RTR+1, 2000 % 256)))	// 200 ms
+    return status;
+
+// Initialize retry count
+
+  if ((status = W5200_write_register(W5200_RCR, 10)))
+    return status;
+
+// Initialize interrupt high time
+
+  if ((status = W5200_write_register(W5200_INTLEVEL, 1)))	// 1 clock
+    return status;
+
+// Initialize socket interrupt mask register
+
+  if ((status = W5200_write_register(W5200_IMR2, 0)))		// All disabled
+    return status;
+
+// Initialize memory size registers
+
+  for (s = 0; s < W5200_MAX_SOCKETS; s++)
+  {
+    if ((status = W5200_write_register(W5200_Sn_RXMEMSIZE(s), 2))) // 2K RAM
+      return status;
+
+    if ((status = W5200_write_register(W5200_Sn_TXMEM_SIZE(s), 2))) // 2K RAM
+      return status;
+  }
 
   return status;
 }
@@ -123,8 +169,38 @@ int W5200_get_linkstate(int *linkstate)
   return status;
 }
 
-void W5200_tick(void)
+int W5200_udp_open(int socket, uint16_t sourceport)
 {
-  if (delaycounter)
-    delaycounter--;
+  int status = 0;
+  uint8_t data;
+
+  if ((status = W5200_write_register(W5200_Sn_MR(socket), W5200_Sn_MR_UDP)))
+    return status;
+
+  if ((status = W5200_write_register(W5200_Sn_PORT(socket), sourceport / 256)))
+    return status;
+
+  if ((status = W5200_write_register(W5200_Sn_PORT(socket), sourceport % 256)))
+    return status;
+
+  if ((status = W5200_write_register(W5200_Sn_CR(socket), W5200_Sn_CR_OPEN)))
+    return status;
+
+  delaycounter = 10;
+
+  while (delaycounter > 0)
+  {
+    if ((status = W5200_read_register(W5200_Sn_SR(socket), &data)))
+      return status;
+
+    if (data == W5200_Sn_SR_SOCK_UDP)
+    {
+      delaycounter = 0;
+      return 0;
+    }  
+  }
+
+  errno_r = ETIMEDOUT;
+  return __LINE__;
 }
+
