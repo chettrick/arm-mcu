7dd4366be4f16b980a47a3d7d9300e427f732fe0
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-10-06 12:58:25 +0000

    Changed LEDx to LEDxPIN to avoid clash with leds.h
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3106 06ee2f96-5407-11de-8bab-f3d7589a4122

--- at91sam7s/test_gpio.c
+++ at91sam7s/test_gpio.c
@@ -13,21 +13,21 @@ int main(void)
   cpu_init(DEFAULT_CPU_FREQ);
 
 #ifdef OLIMEX_SAM7_P256
-#define LED1		18
-#define LED2		17
+#define LED1PIN		18
+#define LED2PIN		17
 
   *AT91C_PMC_PCER	= 0x00000004;	// Enable Port A peripheral clock
-  *AT91C_PIOA_PER	= 1 << LED1;	// LED1 pin is GPIO
-  *AT91C_PIOA_PPUDR	= 1 << LED1;	// LED1 pin does not need internal pullup
-  *AT91C_PIOA_OER	= 1 << LED1;	// LED1 pin is output
-  *AT91C_PIOA_OWER	= 1 << LED1;	// LED1 pin is synchronous output
-  *AT91C_PIOA_MDER	= 1 << LED1;	// LED1 pin is open drain output
+  *AT91C_PIOA_PER	= 1 << LED1PIN;	// LED1 pin is GPIO
+  *AT91C_PIOA_PPUDR	= 1 << LED1PIN;	// LED1 pin does not need internal pullup
+  *AT91C_PIOA_OER	= 1 << LED1PIN;	// LED1 pin is output
+  *AT91C_PIOA_OWER	= 1 << LED1PIN;	// LED1 pin is synchronous output
+  *AT91C_PIOA_MDER	= 1 << LED1PIN;	// LED1 pin is open drain output
 
-  *AT91C_PIOA_PER	= 1 << LED2;	// LED2 pin is GPIO
-  *AT91C_PIOA_PPUDR	= 1 << LED2;	// LED2 pin does not need internal pullup
-  *AT91C_PIOA_OER	= 1 << LED2;	// LED2 pin is output
-  *AT91C_PIOA_OWER	= 1 << LED2;	// LED2 pin is synchronous output
-  *AT91C_PIOA_MDER	= 1 << LED2;	// LED2 pin is open drain output
+  *AT91C_PIOA_PER	= 1 << LED2PIN;	// LED2 pin is GPIO
+  *AT91C_PIOA_PPUDR	= 1 << LED2PIN;	// LED2 pin does not need internal pullup
+  *AT91C_PIOA_OER	= 1 << LED2PIN;	// LED2 pin is output
+  *AT91C_PIOA_OWER	= 1 << LED2PIN;	// LED2 pin is synchronous output
+  *AT91C_PIOA_MDER	= 1 << LED2PIN;	// LED2 pin is open drain output
 
   for (i = 0;; i++)
   {
