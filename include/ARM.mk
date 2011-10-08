35126f3911287b9d7c747fe191b7de00ef205233
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-10-08 19:23:07 +0000

    Removed -fdata-sections
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3113 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/ARM.mk
+++ include/ARM.mk
@@ -33,7 +33,7 @@ CONFIGFLAGS	?=
 DEBUGFLAGS	?= -g
 OPTFLAGS	?= -O0
 IOFLAGS		?= -DINTEGER_STDIO
-CFLAGS		+= -Wall -mcpu=$(CPU) $(CPUFLAGS) -D$(MCU) -D$(BOARDNAME) -fdata-sections -ffunction-sections
+CFLAGS		+= -Wall -mcpu=$(CPU) $(CPUFLAGS) -D$(MCU) -D$(BOARDNAME) -ffunction-sections
 CFLAGS		+= -I$(ARMSRC)/include -I$(MCUDIR)
 CFLAGS		+= $(CONFIGFLAGS) $(DEBUGFLAGS) $(OPTFLAGS) $(IOFLAGS) $(DEBUG) $(EXTRAFLAGS)
 LDFLAGS		+= -nostartfiles -T$(LINKERSCRIPT) -L$(MCUDIR) -l$(MCU) -Wl,-Map=$*.map,--cref,--entry=_start,--gc-sections $(EXTRAOBJS)
