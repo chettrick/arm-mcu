db9e07666aa7629163b560775c98785b51843a81
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-10-15 19:02:34 +0000

    Moved delaycounter from W5200.c Added some data structures for future DHCP implementation.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3159 06ee2f96-5407-11de-8bab-f3d7589a4122

--- wiznet/wiznet.c
+++ wiznet/wiznet.c
@@ -3,17 +3,42 @@
 // $Id$
 
 #include <errno.h>
+#include <math.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <wiznet.h>
 
 #define errno_r (*(__errno()))
 
-int wiznet_bootp(ipv4address_t address,
-                 ipv4address_t subnet,
-                 ipv4address_t gateway,
-                 ipv4address_t nameserver)
+typedef struct
 {
-  int status = 0;
+  uint8_t op;
+  uint8_t htype;
+  uint8_t hlen;
+  uint8_t hops;
+  uint32_t xid;
+  uint16_t secs;
+  uint16_t flags;
+  ipv4address_t ciaddr;
+  ipv4address_t yiaddr;
+  ipv4address_t siaddr;
+  ipv4address_t giaddr;
+  uint8_t chaddr[16];
+  char sname[64];
+  char file[128];
+  uint8_t vend[64];
+} __attribute__((packed)) bootp_message_t;
 
-  return status;
+static const ipv4address_t broadcast_address = { 255, 255, 255, 255 };
+static const ipv4address_t zero_address	= { 0, 0, 0, 0 };
+
+volatile uint32_t wiznet_delaycounter = 0;
+
+void wiznet_tick(void)
+{
+  if (wiznet_delaycounter)
+    wiznet_delaycounter--;
 }
