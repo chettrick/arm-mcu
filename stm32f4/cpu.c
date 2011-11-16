1d965045840848aed1095d053e8cf3dc2c8a199f
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-16 15:22:52 +0000

    Copied from stm32
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3324 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ stm32f4/cpu.c
@@ -0,0 +1,15 @@
+/* Initialize CPU core */
+
+// $Id: cpu.c 3016 2011-09-19 10:44:24Z svn $
+
+static const char revision[] = "$Id: cpu.c 3016 2011-09-19 10:44:24Z svn $";
+
+#include <cpu.h>
+
+void cpu_init(unsigned long int frequency)
+{
+  __use_custom_syscalls();
+
+  SystemInit();
+  SystemCoreClockUpdate();
+}
