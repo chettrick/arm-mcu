22097f3e5d8c136ba6274db4e5b6edc1f96ece1b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-21 13:12:46 +0000

    Copied from STM32 and updated for STM32F4
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3346 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ stm32f4/cpu.h
@@ -0,0 +1,49 @@
+/* ARM microcontroller device specific definitions and header files */
+
+// $Id: cpu.h 3165 2011-10-17 10:55:40Z svn $
+
+#include <device.h>
+#include <gpiopins.h>
+#include <leds.h>
+#include <serial.h>
+#include <spi.h>
+#include <stm32f4xx.h>
+
+#define errno_r			(*(__errno()))
+
+#ifndef TRUE
+#define TRUE			1
+#endif
+
+#ifndef FALSE
+#define FALSE			0
+#endif
+
+#define CPUFREQ			SystemCoreClock
+
+#ifndef DEFAULT_CPU_FREQ
+#ifdef stm32f407vg
+#define DEFAULT_CPU_FREQ	168000000
+#endif
+#endif
+
+#ifndef CONSOLE_PORT
+#ifdef STM32F4_DISCOVERY
+#define CONSOLE_PORT		"com1:115200,n,8,1"
+#endif
+
+#ifdef INTEGER_STDIO
+#define printf(...)		iprintf(__VA_ARGS__)
+#define fprintf(...)		fiprintf(__VA_ARGS__)
+#define sprintf(...)		siprintf(__VA_ARGS__)
+#define snprintf(...)		sniprintf(__VA_ARGS__)
+#define asprintf(...)		asiprintf(__VA_ARGS__)
+#define asnprintf(...)		asniprintf(__VA_ARGS__)
+
+#define scanf(...)		iscanf(__VA_ARGS__)
+#define sscanf(...)		siscanf(__VA_ARGS__)
+#define fscanf(...)		fiscanf(__VA_ARGS__)
+#endif
+
+extern void __use_custom_syscalls(void);
+extern void cpu_init(unsigned long int frequency);
