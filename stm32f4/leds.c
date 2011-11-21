3f2f9beecf1fca5bd9bc41684f5c7f534ec8078d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-21 13:07:57 +0000

    Copied from stm32 and modified for stm32f4
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3342 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ stm32f4/leds.c
@@ -0,0 +1,49 @@
+/* Abstract services for controlling LED's */
+
+// $Id: leds.c 2988 2011-09-06 08:28:59Z svn $
+
+#include <cpu.h>
+
+// Initialize the LED's (to the off state)
+
+void LEDS_initialize(void)
+{
+#ifdef STM32F4_DISCOVERY
+  gpiopin_configure(GPIOPIN60, GPIOPIN_OUTPUT);
+  gpiopin_configure(GPIOPIN61, GPIOPIN_OUTPUT);
+  gpiopin_configure(GPIOPIN62, GPIOPIN_OUTPUT);
+  gpiopin_configure(GPIOPIN63, GPIOPIN_OUTPUT);
+#endif
+
+  LEDS_set(0);						// Turn off all LEDs at startup
+}
+
+// Get LED states.  A 1 bit in the result indicates the LED is on.
+// A 0 bit in the result indicates the LED is off.
+
+unsigned long int LEDS_get(void)
+{
+  unsigned long int result = 0;
+
+#ifdef STM32F4_DISCOVERY
+  result += GPIOPIN60IN;
+  result += GPIOPIN61IN << 1;
+  result += GPIOPIN62IN << 2;
+  result += GPIOPIN63IN << 3;
+#endif
+
+  return result;
+}
+
+// Set LED states.  A 1 bit in the mask indicates the LED should be turned on. 
+// A 0 bit in the mask indicates the LED should be turned off.
+
+void LEDS_set(unsigned long int mask)
+{
+#ifdef STM32F4_DISCOVERY
+  GPIOPIN60OUT = mask;
+  GPIOPIN61OUT = mask >> 1;
+  GPIOPIN62OUT = mask >> 2;
+  GPIOPIN63OUT = mask >> 3;
+#endif
+}
