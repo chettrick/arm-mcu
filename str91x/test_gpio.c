1f6fc8e222f611c94b1901a3045d2a839a1ef64b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-24 16:24:12 +0000

    Added support for pushbutton and joystick inputs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3387 06ee2f96-5407-11de-8bab-f3d7589a4122

--- str91x/test_gpio.c
+++ str91x/test_gpio.c
@@ -14,8 +14,13 @@ int main(void)
   GPIO_InitTypeDef config;
   unsigned long int i;
 
+  SCU_APBPeriphClockConfig(__GPIO7, ENABLE);	// Turn on GPIO7 clock
+  SCU_APBPeriphReset(__GPIO7, DISABLE);		// Let GPIO7 out of reset
+
   SCU_APBPeriphClockConfig(__GPIO9, ENABLE);	// Turn on GPIO9 clock
   SCU_APBPeriphReset(__GPIO9, DISABLE);		// Let GPIO9 out of reset
+
+  // Initialize LED output pins
  
   GPIO_StructInit(&config);
   config.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3;
@@ -24,7 +29,44 @@ int main(void)
   config.GPIO_Alternate = GPIO_OutputAlt1;	// LED pins are GPIO outputs
   GPIO_Init(GPIO9, &config);
 
+  // Initialize joystick input pins
+
+  GPIO_StructInit(&config);
+  config.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7;
+  config.GPIO_Direction = GPIO_PinInput;	// Joystick pins are inputs
+  config.GPIO_Alternate = GPIO_InputAlt1;	// Joystick pins are GPIO inputs
+  config.GPIO_IPInputConnected = GPIO_IPInputConnected_Disable;
+  GPIO_Init(GPIO7, &config);
+
   for (i = 0;; i++)
-    GPIO_Write(GPIO9, ~(i >> 18));
+  {
+    // Speed up flashing if joystick is UP
+
+    if ((GPIO_Read(GPIO7) >> 4) == 8)
+    {
+      GPIO_Write(GPIO9, ~(i >> 15));
+    }
+
+    // Slow down flashing if joystick is DOWN
+
+    else if ((GPIO_Read(GPIO7) >> 4) == 2)
+    {
+      GPIO_Write(GPIO9, ~(i >> 17));
+    }
+
+    // Turn off LED's if PB3 is pressed
+
+    else if ((GPIO_Read(GPIO7) >> 4) == 10)
+    {
+      GPIO_Write(GPIO9, 0xFF);
+    }
+
+    // Normal flashing
+
+    else
+    {
+      GPIO_Write(GPIO9, ~(i >> 16));
+    }
+  }
 #endif
 }
