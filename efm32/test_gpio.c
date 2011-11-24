07f4ad9450414eaf4befa0b87b523ac959a0b12e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-24 13:15:17 +0000

    Added code to support user buttons
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3375 06ee2f96-5407-11de-8bab-f3d7589a4122

--- efm32/test_gpio.c
+++ efm32/test_gpio.c
@@ -13,6 +13,8 @@ int main(void)
   cpu_init(DEFAULT_CPU_FREQ);
 
 #ifdef EFM32_G8XX_STK
+  gpiopin_configure(25, GPIOPIN_INPUT);
+  gpiopin_configure(26, GPIOPIN_INPUT);
   gpiopin_configure(32, GPIOPIN_OUTPUT);
   gpiopin_configure(33, GPIOPIN_OUTPUT);
   gpiopin_configure(34, GPIOPIN_OUTPUT);
@@ -20,10 +22,35 @@ int main(void)
 
   for (i = 0;; i++)
   {
-    GPIOPIN32OUT = i >> 16;
-    GPIOPIN33OUT = i >> 17;
-    GPIOPIN34OUT = i >> 18;
-    GPIOPIN35OUT = i >> 19;
+    // Speed up flashing if PB0 is pressed
+
+    if (!GPIOPIN25IN)
+    {
+      GPIOPIN32OUT = i >> 16;
+      GPIOPIN33OUT = i >> 17;
+      GPIOPIN34OUT = i >> 18;
+      GPIOPIN35OUT = i >> 19;
+    }
+
+    // Turn off LED's if PB1 is pressed
+
+    else if (!GPIOPIN26IN)
+    {
+      GPIOPIN32OUT = 0;
+      GPIOPIN33OUT = 0;
+      GPIOPIN34OUT = 0;
+      GPIOPIN35OUT = 0;
+    }
+
+    // Normal flashing
+
+    else
+    {
+      GPIOPIN32OUT = i >> 17;
+      GPIOPIN33OUT = i >> 18;
+      GPIOPIN34OUT = i >> 19;
+      GPIOPIN35OUT = i >> 20;
+    }
   }
 #endif
 }
