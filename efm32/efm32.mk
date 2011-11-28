586ef13dace85870acfd92c0d9d960be185ab209
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-28 13:24:35 +0000

    Removed -DEFM32 from CFLAGS
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3446 06ee2f96-5407-11de-8bab-f3d7589a4122

--- efm32/efm32.mk
+++ efm32/efm32.mk
@@ -8,7 +8,7 @@ TEXTBASE	?= 0x00000000
 
 CMSIS		= $(MCUDIR)/CMSIS
 
-CFLAGS		+= -DEFM32 -I$(CMSIS)/include
+CFLAGS		+= -I$(CMSIS)/include
 LDFLAGS		+= -Ttext $(TEXTBASE)
 
 # Board specific macro definitions
