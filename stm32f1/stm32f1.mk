c0ee9971fcf9590e7b72b16d9246657a4ea27f77
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-28 13:56:41 +0000

    Moved stlink and stm32flash rules to ARM.mk
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3451 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f1/stm32f1.mk
+++ stm32f1/stm32f1.mk
@@ -4,6 +4,7 @@
 
 CPU		= cortex-m3
 CPUFLAGS	= -mthumb
+FLASHWRITEADDR	= 0x08000000
 TEXTBASE	?= 0x00000000
 
 CMSIS		= $(MCUDIR)/CMSIS
@@ -11,37 +12,47 @@ CMSIS		= $(MCUDIR)/CMSIS
 CFLAGS		+= -DSTM32F10X -DUSE_STDPERIPH_DRIVER -I$(CMSIS)/include
 LDFLAGS		+= -Ttext $(TEXTBASE)
 
-LIBOBJS		= cpu.o gpiopins.o leds.o serial.o spi.o
-
-FLASHWRITEADDR	= 0x08000000
-
-# All STM32F1 parts can use the same flash script
-
 OPENOCDFLASH	= $(MCUDIR)/stm32f1.flashocd
 
-# STM32 Value Line Discovery board has built-in ST-Link/V1
+# Board specific macro definitions
+
+ifeq ($(BOARDNAME), OLIMEX_STM32_P103)
+MCU             = stm32f103rb
+endif
+
+ifeq ($(BOARDNAME), OLIMEX_STM32_P107)
+MCU             = stm32f107rb
+endif
 
 ifeq ($(BOARDNAME), STM32_VALUE_LINE_DISCOVERY)
+MCU             = stm32f100rb
+
+# STM32 Value Line Discovery board has built-in ST-Link/V1
 STLINKGDBIF	= --stlinkv1 --device=/dev/stlink-v1
 
 ifeq ($(shell uname), Linux)
 STLINKDEV	?= /dev/stlink-v1
 STLINKFLASH	?= stlink-v1-flash
 endif
-
-ifeq ($(findstring CYGWIN, $(shell uname)), CYGWIN)
-STLINKCLI	?= ST-LINK_CLI.exe
-endif
 endif
 
-STM32FLASH	?= stm32flash
-STM32FLASH_PORT	?= /dev/ttyS0
+ifeq ($(BOARDNAME), W5200E01_M3)
+MCU             = stm32f103c8
+endif
 
-.PHONY:		clean_$(MCU) reallyclean_$(MCU) distclean_$(MCU) lib
+# Include MCU specific make file
 
-.SUFFIXES:	.flashstlink .stm32flash
+include $(MCU).mk
+
+# Phony targets
+
+.PHONY:		default lib clean_$(MCU) reallyclean_$(MCU) distclean_$(MCU)
+
+default: lib
 
 # Build processor dependent support library
+
+LIBOBJS		= cpu.o gpiopins.o leds.o serial.o spi.o
 
 lib$(MCU).a: $(LIBOBJS)
 	$(AR) crs lib$(MCU).a $(LIBOBJS)
@@ -50,23 +61,6 @@ lib$(MCU).a: $(LIBOBJS)
 	$(MAKE) otherlibs
 
 lib: lib$(MCU).a
-
-# Define a suffix rule for programming the flash with stlink
-
-ifeq ($(shell uname), Linux)
-.bin.flashstlink:
-	$(STLINKFLASH) $(STLINKDEV) program=$< reset run
-endif
-
-ifeq ($(findstring CYGWIN, $(shell uname)), CYGWIN)
-.bin.flashstlink:
-	$(STLINKCLI) -c SWD -ME -P $< $(FLASHWRITEADDR) -Rst
-endif
-
-# Define a suffix rule for programming the flash with serial boot loader and stm32flash
-
-.bin.stm32flash:
-	$(STM32FLASH) -w $< -v -g 0x0 $(STM32FLASH_PORT)
 
 # Clean out working files
 
