795f9b6dcac79f1894f2c249ddaa4b897c1f117f
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-28 12:44:44 +0000

    Moved most to efm32.mk
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3433 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ efm32/efm32.mk
@@ -0,0 +1,48 @@
+# Processor dependent make definitions
+
+# $Id$
+
+CPU		= cortex-m3
+CPUFLAGS	= -mthumb
+TEXTBASE	?= 0x00000000
+
+CMSIS		= $(MCUDIR)/CMSIS
+
+CFLAGS		+= -DEFM32 -I$(CMSIS)/include
+LDFLAGS		+= -Ttext $(TEXTBASE)
+
+LIBOBJS		= cpu.o gpiopins.o leds.o serial.o
+
+# Board specific macro definitions
+
+ifeq ($(BOARDNAME), EFM32_G8XX_STK)
+MCU		= efm32g890f128
+endif
+
+# Include MCU specific make file
+
+include $(MCU).mk
+
+# Phony targets
+
+.PHONY:		default lib clean_$(MCU) reallyclean_$(MCU) distclean_$(MCU)
+
+default: lib$(MCU).a
+
+# Build processor dependent support library
+
+lib$(MCU).a: $(LIBOBJS)
+	$(AR) crs lib$(MCU).a $(LIBOBJS)
+	for F in $(CMSIS)/source/*.c ; do $(MAKE) $${F%.c}.o ; done
+	$(AR) crs lib$(MCU).a $(CMSIS)/source/*.o
+	$(MAKE) otherlibs
+
+lib: lib$(MCU).a
+
+# Clean out working files
+
+clean_$(MCU):
+
+reallyclean_$(MCU): clean_$(MCU)
+
+distclean_$(MCU): reallyclean_$(MCU)
--- efm32/efm32g890f128.mk
+++ efm32/efm32g890f128.mk
@@ -2,33 +2,5 @@
 
 # $Id$
 
-CPU		= cortex-m3
-CPUFLAGS	= -mthumb
-TEXTBASE	?= 0x00000000
-
-CMSIS		= $(MCUDIR)/CMSIS
-
-CFLAGS		+= -DEFM32 -DEFM32G890F128 -I$(CMSIS)/include
-LDFLAGS		+= -Ttext $(TEXTBASE)
-
-LIBOBJS		= cpu.o gpiopins.o leds.o serial.o
-
-.PHONY:		clean_$(MCU) reallyclean_$(MCU) distclean_$(MCU) lib
-
-# Build processor dependent support library
-
-lib$(MCU).a: $(LIBOBJS)
-	$(AR) crs lib$(MCU).a $(LIBOBJS)
-	for F in $(CMSIS)/source/*.c ; do $(MAKE) $${F%.c}.o ; done
-	$(AR) crs lib$(MCU).a $(CMSIS)/source/*.o
-	$(MAKE) otherlibs
-
-lib: lib$(MCU).a
-
-# Clean out working files
-
-clean_$(MCU):
-
-reallyclean_$(MCU): clean_$(MCU)
-
-distclean_$(MCU): reallyclean_$(MCU)
+MCU		= efm32g890f128
+CFLAGS		+= -DEFM32G890F128 
