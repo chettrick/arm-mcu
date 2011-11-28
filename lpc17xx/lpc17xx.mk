52cdcd33eb8e1b6210730c8ae0ed2206d71cb0e2
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-28 13:53:09 +0000

    Moved flashisp, flashmbed, flashusb stuff to ARM.mk
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3449 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc17xx/lpc17xx.mk
+++ lpc17xx/lpc17xx.mk
@@ -12,27 +12,15 @@ CMSIS		= $(MCUDIR)/CMSIS
 CFLAGS		+= -DLPC17XX -I$(CMSIS)/include
 LDFLAGS		+= -Ttext $(TEXTBASE)
 
-LPC21ISP	?= lpc21isp
-LPC21ISPDEV	?= /dev/ttyS0
-LPC21ISPBAUD	?= 115200
-LPC21ISPCLOCK	?= 14746
-LPC21ISPFLAGS	?= -control
-
 # Board specific macro definitions
-
-ifeq ($(BOARDNAME), MBED_LPC1768)
-MBED		?= /media/MBED
-endif
 
 ifeq ($(BOARDNAME), BLUEBOARD_LPC1768_H)
 JLINKMCU	= lpc1768
-USBBOOT		?= /media/LPC17xx
 endif
 
 # Phony targets
 
 .PHONY:		default lib clean_$(MCU) reallyclean_$(MCU) distclean_$(MCU)
-.SUFFIXES:	.flashisp .flashmbed .flashusb
 
 default: lib
 
@@ -47,27 +35,6 @@ lib$(MCU).a: $(LIBOBJS)
 	$(MAKE) otherlibs
 
 lib: lib$(MCU).a
-
-# Define a suffix rule for programming the flash with lpc21isp
-
-.hex.flashisp:
-	$(LPC21ISP) $(LPC21ISPFLAGS) $< $(LPC21ISPDEV) $(LPC21ISPBAUD) $(LPC21ISPCLOCK)
-
-# Define a suffix rule for installing to an mbed board
-
-.bin.flashmbed:
-	test -d $(MBED) -a -w $(MBED)
-	cp $< $(MBED)
-	sync
-	@echo -e "\nPress RESET on the mbed LPC1768 board to start $<\n"
-
-# Define a suffix rule for installing via the NXP USB boot loader
-
-.bin.flashusb:
-	test -d $(USBBOOT) -a -w $(USBBOOT)
-	cp $< $(USBBOOT)/firmware.bin
-	sync
-	@echo -e "\nPress RESET on the target board to start $<\n"
 
 # Clean out working files
 
