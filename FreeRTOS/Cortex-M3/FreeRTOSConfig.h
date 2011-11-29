3f91fb86f4704b1727ca178e2cb53dc0425040e7
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-11-29 15:31:56 +0000

    Removed obsolete INCLUDE_vTaskCleanUpResources from FreeRTOSConfig.h. Added new (7.0.2) INCLUDE_xTaskGetIdleTaskHandle to FreeRTOSConfig.h. Added new (7.0.2) INCLUDE_xTimerGetTimerDaemonTaskHandle to FreeRTOSConfig.h. Added new (7.0.2) INCLUDE_pcTaskGetTaskName to FreeRTOSConfig.h.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3473 06ee2f96-5407-11de-8bab-f3d7589a4122

--- FreeRTOS/Cortex-M3/FreeRTOSConfig.h
+++ FreeRTOS/Cortex-M3/FreeRTOSConfig.h
@@ -109,12 +109,14 @@ to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet	1
 #define INCLUDE_uxTaskPriorityGet	1
 #define INCLUDE_vTaskDelete		1
-#define INCLUDE_vTaskCleanUpResources	0
 #define INCLUDE_vTaskSuspend		1
 #define INCLUDE_vTaskDelayUntil		1
 #define INCLUDE_vTaskDelay		1
 #define INCLUDE_uxTaskGetStackHighWaterMark	1
 #define INCLUDE_xTaskGetCurrentTaskHandle	1
+#define INCLUDE_xTaskGetIdleTaskHandle	1
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle	1
+#define INCLUDE_pcTaskGetTaskName	1
 
 /*-----------------------------------------------------------
  * Ethernet configuration.
