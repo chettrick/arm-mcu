5bd27a5a09da23a776046b919266d9173f1736d9
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-12-02 10:49:25 +0000

    Use find to pick up all object files
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3501 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f4/libs/stm32f4libs.mk
+++ stm32f4/libs/stm32f4libs.mk
@@ -13,12 +13,10 @@ CMSIS		= $(PERIPHLIBDIR)/Libraries/CMSIS
 STM32F4LIB	= $(PERIPHLIBDIR)/Libraries/STM32F4xx_StdPeriph_Driver
 STM32F4TEMPLATE	= $(PERIPHLIBDIR)/Project/STM32F4xx_StdPeriph_Templates
 
-CFLAGS		+= -DUSE_STDPERIPH_DRIVER -I$(STM32F4LIB)/inc
+CFLAGS		+= -DUSE_STDPERIPH_DRIVER -I$(STM32F4LIB)/inc -I$(STM32F4TEMPLATE)
 CFLAGS		+= -I$(CMSIS)/Include -I$(CMSIS)/Device/ST/STM32F4xx/Include
-CFLAGS		+= -I$(STM32F4TEMPLATE)
 
 stm32f4libs:
 	$(MAKE) $(STM32F4TEMPLATE)/system_stm32f4xx.o
-	$(AR) crs lib$(MCU).a $(STM32F4TEMPLATE)/system_stm32f4xx.o
 	for F in $(STM32F4LIB)/src/*.c ; do $(MAKE) $${F%.c}.o ; done
-	$(AR) crs lib$(MCU).a $(STM32F4LIB)/src/*.o
+	find $(PERIPHLIBDIR) -type f -name '*.o' -exec $(AR) crs lib$(MCU).a {} ";"
