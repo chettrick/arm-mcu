8f8cdd90cc5f07d26880a49839d6e7f968ac83dc
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2011-12-02 09:27:48 +0000

    Imported STM32F4 Standard Peripheral Library in toto
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3494 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ stm32f4/libs/stm32f4libs.mk
@@ -0,0 +1,25 @@
+# Makefile for STM32F4 libraries imported from ST and other sources
+
+# $Id$
+
+LIBSDIR		= $(ARMSRC)/stm32f4/libs
+
+# Definitions for the STM32F4 Standard Peripheral Library
+
+PERIPHLIBVER	= V1.0.0
+PERIPHLIBNAME	= STM32F4xx_DSP_StdPeriph_Lib
+PERIPHLIBDIR	= $(LIBSDIR)/$(PERIPHLIBNAME)_$(PERIPHLIBVER)
+CMSIS		= $(PERIPHLIBDIR)/Libraries/CMSIS
+CMSISTEMPLATE	= $(CMSIS)/Templates
+STM32F4LIB	= $(PERIPHLIBDIR)/Libraries/STM32F4xx_StdPeriph_Driver
+STM32F4TEMPLATE	= $(PERIPHLIBDIR)/Project/STM32F4xx_StdPeriph_Templates
+
+CFLAGS		+= -DUSE_STDPERIPH_DRIVER -I$(STM32F4LIB)/inc
+CFLAGS		+= -I$(CMSIS)/Include -I$(CMSIS)/Device/ST/STM32F4xx/Include
+CFLAGS		+= -I$(STM32F4TEMPLATE)
+
+stm32f4libs:
+	$(MAKE) $(CMSISTEMPLATE)/system_stm32f4xx.o
+	$(AR) crs lib$(MCU).a $(CMSISTEMPLATE)/system_stm32f4xx.o
+	for F in $(STM32F4LIB)/src/*.c ; do $(MAKE) $${F%.c}.o ; done
+	$(AR) crs lib$(MCU).a $(STM32F4LIB)/src/*.o
