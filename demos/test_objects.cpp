99be2c494c4d3b1857bfc7215f39daf5aa4b826a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-04-23 08:07:17 +0000

    Renamed systick.cpp to test_objects.cpp
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3730 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ demos/test_objects.cpp
@@ -0,0 +1,110 @@
+/* Simple C++ object test program */
+
+// $Id$
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <stdio.h>
+
+// Define an object class
+
+class testclass1
+{
+public:
+
+  testclass1(unsigned int d)
+  {
+    this->param = d;
+  }
+
+  unsigned int param;
+};
+
+// Define another object class
+
+class testclass2
+{
+public:
+
+  testclass2(unsigned int e)
+  {
+    this->param = e;
+  }
+
+  unsigned int param;
+};
+
+// Create some global objects--constructors should be called from startup.S
+// before main() is called
+
+testclass1 mytest1 = testclass1(1);
+
+testclass1 mytest2(2);
+
+testclass2 mytest3 = testclass2(3);
+
+testclass2 mytest4(4);
+
+int main(void)
+{
+  cpu_init(DEFAULT_CPU_FREQ);
+
+  serial_stdio((char *) CONSOLE_PORT);
+
+  printf("\033[H\033[2J%s C++ Object Test (" __DATE__ " " __TIME__ ")\n\n", MCUFAMILYNAME);
+  puts(revision);
+  printf("\nCPU Freq:%ld Hz  Compiler:%s %s %s\n\n", CPUFREQ, __COMPILER__, __VERSION__, __ABI__);
+
+// Create some local objects
+
+  testclass1 mytest5 = testclass1(5);
+
+  testclass1 mytest6(6);
+
+  testclass2 mytest7 = testclass2(7);
+
+  testclass2 mytest8(8);
+
+// Test the objects
+
+  if (mytest1.param == 1)
+  {
+    puts("Object mytest1 was created successfully!");
+  }
+
+  if (mytest2.param == 2)
+  {
+    puts("Object mytest2 was created successfully!");
+  }
+
+  if (mytest3.param == 3)
+  {
+    puts("Object mytest3 was created successfully!");
+  }
+
+  if (mytest4.param == 4)
+  {
+    puts("Object mytest4 was created successfully!");
+  }
+
+  if (mytest5.param == 5)
+  {
+    puts("Object mytest5 was created successfully!");
+  }
+
+  if (mytest6.param == 6)
+  {
+    puts("Object mytest6 was created successfully!");
+  }
+
+  if (mytest7.param == 7)
+  {
+    puts("Object mytest7 was created successfully!");
+  }
+
+  if (mytest8.param == 8)
+  {
+    puts("Object mytest8 was created successfully!");
+  }
+}
