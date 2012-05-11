b13e00ad6e02b480b93af8041642bf5d763fb31a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-05-11 08:32:30 +0000

    Added a missing word to the comments
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3811 06ee2f96-5407-11de-8bab-f3d7589a4122

--- include/socket.h
+++ include/socket.h
@@ -1,7 +1,7 @@
 /* TCP/IP socket API (Application Programming Interface.  These functions are */
 /* implemented in the various network device drivers and server to bind those */
 /* drivers to the ARM device infrastructure.  When the following functions    */
-/* implemented and registered to a file discriptor using the                  */
+/* are implemented and registered to a file discriptor using the              */
 /* device_register_char() function, the TCP/IP socket can then be completely  */
 /* manipulated via the formal file I/O functions such as open(), read() and   */
 /* like.                                                                      */
