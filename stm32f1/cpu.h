9f4059ca9790b96adc37e719d15584eaeed25e81
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-05-15 09:54:08 +0000

    Moved the definition of CONSOLE_PORT from cpu.h to stm32f1.mk.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3848 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f1/cpu.h
+++ stm32f1/cpu.h
@@ -15,42 +15,7 @@
 #include <stm32f10x.h>
 
 #define CPUFREQ			SystemCoreClock
-
-#ifndef DEFAULT_CPU_FREQ
-#ifdef stm32f100rb
-#define DEFAULT_CPU_FREQ	24000000
-#endif
-
-#ifdef stm32f103c8
-#define DEFAULT_CPU_FREQ	72000000
-#endif
-
-#ifdef stm32f103rb
-#define DEFAULT_CPU_FREQ	72000000
-#endif
-
-#ifdef stm32f107rb
-#define DEFAULT_CPU_FREQ	72000000
-#endif
-#endif
-
-#ifndef CONSOLE_PORT
-#ifdef OLIMEX_STM32_P103
-#define CONSOLE_PORT		"com2:115200,n,8,1"
-#endif
-
-#ifdef OLIMEX_STM32_P107
-#define CONSOLE_PORT		"com3:115200,n,8,1"
-#endif
-
-#ifdef STM32_VALUE_LINE_DISCOVERY
-#define CONSOLE_PORT		"com1:115200,n,8,1"
-#endif
-#endif
-
-#ifdef W5200E01_M3
-#define CONSOLE_PORT		"com1:115200,n,8,1"
-#endif
+#define DEFAULT_CPU_FREQ	0
 
 _BEGIN_STD_C
 
