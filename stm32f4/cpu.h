4a771c1121e42e65e2afe3d03ee808f16049b86d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-05-15 08:44:32 +0000

    Moved definitions for DEFAULT_CPU_FREQ and CONSOLE_PORT from cpu.h to stm32f4.mk.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@3825 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f4/cpu.h
+++ stm32f4/cpu.h
@@ -16,18 +16,6 @@
 
 #define CPUFREQ			SystemCoreClock
 
-#ifndef DEFAULT_CPU_FREQ
-#ifdef stm32f407vg
-#define DEFAULT_CPU_FREQ	168000000
-#endif
-#endif
-
-#ifndef CONSOLE_PORT
-#ifdef STM32F4_DISCOVERY
-#define CONSOLE_PORT		"com2:115200,n,8,1"
-#endif
-#endif
-
 _BEGIN_STD_C
 
 extern void cpu_init(unsigned long int frequency);
