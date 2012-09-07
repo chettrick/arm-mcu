546ffbbdc90e0b83b625c3b75dca46e68a6a4e0e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-09-07 10:00:00 +0000

    Switched from the full peripheral library source tree to just what we need.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4223 06ee2f96-5407-11de-8bab-f3d7589a4122

--- stm32f1/libs/stm32f1libs.mk
+++ stm32f1/libs/stm32f1libs.mk
@@ -3,22 +3,14 @@
 # $Id$
 
 LIBSDIR		= $(ARMSRC)/stm32f1/libs
+CMSIS		= $(LIBSDIR)/CMSIS
+DRIVERS		= $(LIBSDIR)/drivers
 
-# Definitions for the STM32F1 Standard Peripheral Library
-
-PERIPHLIBVER	= V3.5.0
-PERIPHLIBNAME	= STM32F10x_StdPeriph_Lib
-PERIPHLIBDIR	= $(LIBSDIR)/$(PERIPHLIBNAME)_$(PERIPHLIBVER)
-CMSIS		= $(PERIPHLIBDIR)/Libraries/CMSIS/CM3
-STM32F1LIB	= $(PERIPHLIBDIR)/Libraries/STM32F10x_StdPeriph_Driver
-STM32F1TEMPLATE	= $(PERIPHLIBDIR)/Project/STM32F10x_StdPeriph_Template
-
-CFLAGS		+= -DUSE_STDPERIPH_DRIVER -I$(STM32F1LIB)/inc -I$(STM32F1TEMPLATE)
-CFLAGS		+= -I$(CMSIS)/Include -I$(CMSIS)/CoreSupport
-CFLAGS		+= -I$(CMSIS)/Include -I$(CMSIS)/DeviceSupport/ST/STM32F10x
+CFLAGS		+= -I$(CMSIS)/CM3/CoreSupport -I$(CMSIS)/CM3/DeviceSupport/ST/STM32F10x
+CFLAGS		+= -DUSE_STDPERIPH_DRIVER -I$(DRIVERS)/inc
 
 stm32f1libs:
-	$(MAKE) $(CMSIS)/CoreSupport/core_cm3.o
-	$(MAKE) $(CMSIS)/DeviceSupport/ST/STM32F10x/system_stm32f10x.o
-	for F in $(STM32F1LIB)/src/*.c ; do $(MAKE) $${F%.c}.o ; done
-	$(FIND) $(PERIPHLIBDIR) -type f -name '*.o' -exec $(AR) crs lib$(MCU).a {} ";"
+	$(MAKE) $(CMSIS)/CM3/CoreSupport/core_cm3.o
+	$(MAKE) $(CMSIS)/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.o
+	for F in $(DRIVERS)/src/*.c ; do $(MAKE) $${F%.c}.o ; done
+	$(FIND) $(LIBSDIR) -type f -name '*.o' -exec $(AR) crs lib$(MCU).a {} ";"
