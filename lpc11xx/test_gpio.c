d37df429f2f1e041d832366016cdf4d53e4d6b2e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-10-01 20:08:16 +0000

    Configure IOCON registers for each of the GPIO outputs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4323 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc11xx/test_gpio.c
+++ lpc11xx/test_gpio.c
@@ -12,10 +12,22 @@ int main(void)
 
   cpu_init(DEFAULT_CPU_FREQ);
 
+#ifdef LPC1114FN28
+  LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16);	// Enable IOCON
+
+  LPC_IOCON->R_PIO1_0 = 0xC1;
+  LPC_IOCON->R_PIO1_1 = 0xC1;
+  LPC_IOCON->R_PIO1_2 = 0xC1;
+  LPC_IOCON->PIO1_4 = 0xC0;
+  LPC_IOCON->PIO1_5 = 0xC0;
+  LPC_IOCON->PIO1_6 = 0xC0;
+  LPC_IOCON->PIO1_7 = 0xC0;
+  LPC_IOCON->PIO1_8 = 0xC0;
+  LPC_IOCON->PIO1_9 = 0xC0;
+
   LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 6);	// Enable GPIO
 
-#ifdef LPC1114FN28
-  LPC_GPIO1->DIR = 0xFF;	// P1.0-P1.7 are outputs
+  LPC_GPIO1->DIR = 0x3F7;	// P1.0-P1.9 are outputs
 
   for (i = 0;; i++)
     LPC_GPIO1->DATA = i;
