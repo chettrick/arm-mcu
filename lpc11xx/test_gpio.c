252c7cfff7cbcda5c394dca066cd917c08c89f23
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2012-12-16 07:00:29 +0000

    Added code to flash an LED on PIO0.7
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4532 06ee2f96-5407-11de-8bab-f3d7589a4122

--- lpc11xx/test_gpio.c
+++ lpc11xx/test_gpio.c
@@ -13,6 +13,8 @@ int main(void)
   cpu_init(DEFAULT_CPU_FREQ);
 
 #ifdef LPC1114FN28
+  LPC_IOCON->PIO0_7 = 0xC0;			// PIO0.7 is GPIO
+
   LPC_IOCON->R_PIO1_0 = 0xC1;			// Configure PIO1 pins for GPIO
   LPC_IOCON->R_PIO1_1 = 0xC1;
   LPC_IOCON->R_PIO1_2 = 0xC1;
@@ -23,9 +25,13 @@ int main(void)
   LPC_IOCON->PIO1_8 = 0xC0;
   LPC_IOCON->PIO1_9 = 0xC0;
 
+  LPC_GPIO0->DIR = 0x80;			// P0.7 is output
   LPC_GPIO1->DIR = 0x3F7;			// P1.0-P1.9 are outputs
 
   for (i = 0;; i++)
+  {
+    LPC_GPIO0->DATA = i >> 12;			// Flash LED
     LPC_GPIO1->DATA = i;
+  }
 #endif
 }
