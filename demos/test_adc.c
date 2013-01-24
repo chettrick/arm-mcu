fea86808b366b1d99dbfaee60f96ffdb6796cf6c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-01-24 15:16:59 +0000

    Added A/D converter test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4639 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ demos/test_adc.c
@@ -0,0 +1,76 @@
+/* Simple A/D Converter Test program */
+
+// $Id$
+
+// Copyright (C)2013, Philip Munts, President, Munts AM Corp.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification,are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Neither the name of Munts AM Corp. nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifdef CONSOLE_CONIO
+#include <conio.h>
+#endif
+
+#define MAX_CHANNELS	8
+
+int main(void)
+{
+  int channel;
+  uint16_t results[MAX_CHANNELS];
+
+  cpu_init(DEFAULT_CPU_FREQ);
+
+  serial_stdio(CONSOLE_PORT);
+
+  printf("\033[H\033[2J%s A/D Converter Test (" __DATE__ " " __TIME__ ")\n\n", MCUFAMILYNAME);
+  puts(revision);
+  printf("\nCPU Freq:%u Hz  Compiler:%s %s %s\n\n", (unsigned int) SystemCoreClock,
+    __COMPILER__, __VERSION__, __ABI__);
+
+  for (channel = 0; channel <= 7; channel++)
+    if (adc_init(channel) < 0)
+      printf("ERROR: adc_init() for channel %d failed, %s\n", channel, strerror(errno));
+
+  putchar('\n');
+
+  for (;;)
+  {
+    for (channel = 0; channel < MAX_CHANNELS; channel++)
+      results[channel] = adc_read(channel);
+
+    for (channel = 0; channel < MAX_CHANNELS; channel++)
+      printf("%05d ", results[channel]);
+
+    putchar('\r');
+  }
+}
