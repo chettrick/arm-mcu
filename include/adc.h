30e9b1d28981cb2e44a47523e66db1fc70b883e7
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-01-24 08:16:37 +0000

    Added adc.h
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4636 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ include/adc.h
@@ -0,0 +1,48 @@
+/* Simple Analog to Digital Converter services */
+
+// $Id$
+
+// Copyright (C)2013, Philip Munts, President, Munts AM Corp.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification,are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Neither the name of Munts AM Corp. nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _ADC_H
+#define _ADC_H
+
+#include <_ansi.h>
+
+_BEGIN_STD_C
+
+// Initialize an A/D input pin
+//   Returns 0 on success or nonzero on failure and sets errno
+
+int adc_init(int channel);
+
+// Read an A/D input
+//   Returns unsigned 16-bit result, left-justified, or zero on error
+
+uint16_t adc_read(int channel);
+
+_END_STD_C
+#endif
