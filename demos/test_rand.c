7b0ad3e537b5ea6ee044f98697a46b9a472af0d4
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-01-30 10:48:13 +0000

    Added random number test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4677 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ demos/test_rand.c
@@ -0,0 +1,79 @@
+/* Minimal Standard Random Number Generator Test program */
+
+// $Id$
+
+// Copyright (C)2013, Philip Munts, President, Munts AM Corp.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification,are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Neither the name of Munts AM Corp. nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+static const char revision[] = "$Id$";
+
+#include <cpu.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifdef CONSOLE_CONIO
+#include <conio.h>
+#endif
+
+#define ITERATIONS	10000
+#define Z1		1
+#define Zn		1043618065
+
+void main(void)
+{
+  int i;
+  unsigned int myseed;
+
+  cpu_init(DEFAULT_CPU_FREQ);
+
+  serial_stdio(CONSOLE_PORT);
+
+  printf("\033[H\033[2J%s Minimal Standard Random Number Generator Test (" __DATE__ " " __TIME__ ")\n\n", MCUFAMILYNAME);
+  puts(revision);
+  printf("\nCPU Freq:%u Hz  Compiler:%s %s %s\n\n", (unsigned int) SystemCoreClock,
+    __COMPILER__, __VERSION__, __ABI__);
+
+  myseed = Z1;
+
+  printf("z1 is %d\n", myseed);
+
+  for (i = 0; i < ITERATIONS; i++)
+    rand_r(&myseed);
+
+  printf("z%d is %d\n\n", ITERATIONS + 1, myseed);
+
+  if (myseed == Zn)
+  {
+    puts("SUCCESS!");
+  }
+  else
+  {
+    printf("FAILURE! Expected z%d is %d\n", ITERATIONS + 1, Zn);
+  }
+}
