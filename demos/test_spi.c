4c87cbb838560f8dac88e9ed0e9951680ff7676a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-02-08 15:02:05 +0000

    Changed exit(1) to return 1
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4716 06ee2f96-5407-11de-8bab-f3d7589a4122

--- demos/test_spi.c
+++ demos/test_spi.c
@@ -74,7 +74,7 @@ int main(void)
   if ((status = spi_master_init(SPI_PORT, 8, 0, 281250, SPI_MSBFIRST)))
   {
     printf("ERROR: spi_master_init() failed at line %d, %s\n", status, strerror(errno));
-    exit(1);
+    return 1;
   }
 
   for (;;)
@@ -87,7 +87,7 @@ int main(void)
     if ((status = spi_master_transfer(SPI_PORT, &commandbyte, 1, &responsebyte, 1)))
     {
       printf("ERROR: spi_master_transfer() failed at line %d, %s\n", status, strerror(errno));
-      exit(1);
+      return 1;
     }
 
     printf("Response was %02X\n\n", responsebyte);
