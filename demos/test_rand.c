b86f38b38102783c3584efa33d819f7f15fa74bb
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-04-11 09:22:14 +0000

    Added support for USB serial port console
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@4844 06ee2f96-5407-11de-8bab-f3d7589a4122

--- demos/test_rand.c
+++ demos/test_rand.c
@@ -48,7 +48,12 @@ void main(void)
 
   cpu_init(DEFAULT_CPU_FREQ);
 
+#ifdef CONSOLE_USB
+  usb_serial_stdio(NULL);
+  getch();
+#else
   serial_stdio(CONSOLE_PORT);
+#endif
 
   printf("\033[H\033[2J%s Minimal Standard Random Number Generator Test (" __DATE__ " " __TIME__ ")\n\n", MCUFAMILYNAME);
   puts(revision);
