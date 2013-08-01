7f8d1ee19743515135db4a2ab0cc639a3db10441
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2013-08-01 09:38:05 +0000

    Added test_semihosting.c
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ARM@5086 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ demos/test_semihosting.c
@@ -0,0 +1,62 @@
+/* Semihosting I/O Test Program */
+
+// $Id$
+
+// Copyright (C)2013, Philip Munts, President, Munts AM Corp.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+static const char revision[] = "$Id$";
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cpu.h>
+
+int main(void)
+{
+  char buf[256];
+
+  cpu_init(DEFAULT_CPU_FREQ);
+
+  semihosting_stdio("semihost::tt");
+
+  printf("\033[H\033[2J%s Semi-Hosting Console Test (" __DATE__ " " __TIME__ ")\n\n", MCUFAMILYNAME);
+  puts(revision);
+  printf("\nCPU Freq:%u Hz  Compiler:%s %s %s\n\n", (unsigned int) SystemCoreClock,
+    __COMPILER__, __VERSION__, __ABI__);
+
+  for (;;)
+  {
+    printf("Enter a command: ");
+    fflush(stdout);
+
+    memset(buf, 0, sizeof(buf));
+    gets(buf);
+
+    printf("You entered => %s\n", buf);
+
+    if (!strcasecmp(buf, "quit")) break;
+  }
+
+  puts("\nEND OF PROGRAM");
+  exit(0);
+}
